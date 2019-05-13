c1d378f446bbe9412cdcd06c5852136c1bf9ff99
peterkinuthia <peterchege442@gmail.com>
2019-05-13 17:06:55 +0300

    product detalis

--- js/change.js
+++ js/change.js
@@ -15,7 +15,7 @@ function validation(event) {
         if (input_field[i - 1].value == '' || text_area.value == '') {
             count = count + 1;
         } else {
-            count = 0;
+            count = 0; 
         }
     }
     if (count != 0 || y == 0) {
@@ -78,3 +78,30 @@ function prev_step4() {
     document.getElementById("active2").style.color = "#00ac00";
     document.getElementById("active3").style.color = "gray";
 }
+
+
+// Progress navbar class
+// Get the container element
+// var btnContainer = document.getElementById("progressbar");
+
+// // Get all buttons with class="btn" inside the container
+// var btns = btnContainer.getElementsByClassName("act");
+
+// // Loop through the buttons and add the active class to the current/clicked button
+// for (var i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function () {
+//         var current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }
+
+
+// var a = document.querySelectorAll(".nav-list");
+// for (var i = 0, length = a.length; i < length; i++) {
+//     a[i].onclick = function () {
+//         var b = document.querySelector(".nav-list li.active");
+//         if (b) b.classList.remove("active");
+//         this.parentNode.classList.add('active');
+//     };
+// }
