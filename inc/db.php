376e71cf33287067437d574bb3b07d4973878409
peterkinuthia <peterchege442@gmail.com>
2019-06-06 15:00:43 +0300

    changed db user name

--- inc/db.php
+++ inc/db.php
@@ -3,7 +3,7 @@
 if ($_SERVER['DOCUMENT_ROOT'] == 'C:/xampp/htdocs') {
 	$db = mysqli_connect('localhost', 'root', '', 'apa');
 } else {
-	$db = mysqli_connect('localhost', 'apa_admin', 'apa_admin', 'vinehubc_apa');
+	$db = mysqli_connect('localhost', 'vinehubc_apa_admin', 'apa_admin', 'vinehubc_apa');
 }
 
 
