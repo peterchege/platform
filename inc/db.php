fec8e429b9ae3ee16821aa79f4990bd062ae33de
anthony-baru <anthonybaru@gmail.com>
2019-06-06 12:32:19 +0300

    Updated sql file with dynamic db.php

--- inc/db.php
+++ inc/db.php
@@ -1,12 +1,18 @@
 <?php
-$db=mysqli_connect('localhost','root','','apa');
 
-if(!$db){
-	echo 'Database connection unsuccessful '.mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT) ;
+if ($_SERVER['DOCUMENT_ROOT'] == 'C:/xampp/htdocs') {
+	$db = mysqli_connect('localhost', 'root', '', 'apa');
+} else {
+	$db = mysqli_connect('localhost', 'apa_admin', 'apa_admin', 'vinehubc_apa');
+}
+
+
+if (!$db) {
+	echo 'Database connection unsuccessful ' . mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 }
 
 date_default_timezone_set("Africa/Nairobi");
-$currentTime=time();
-$dateTime=strftime("%d,%B %Y ",$currentTime);
+$currentTime = time();
+$dateTime = strftime("%d,%B %Y ", $currentTime);
 
-$deadline = $db -> query("SELECT deadline FROM apa_job_posts WHERE id = 11 ");
+$deadline = $db->query("SELECT deadline FROM apa_job_posts WHERE id = 11 ");
