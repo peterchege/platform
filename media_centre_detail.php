c378db697ee56922bdab1c3237a607ecd959dd88
peterkinuthia <peterchege442@gmail.com>
2019-06-06 16:58:12 +0300

    amendement for media centre details

--- media_centre_detail.php
+++ media_centre_detail.php
@@ -59,16 +59,24 @@
                             </i>
                         </p>    
                         <p2>    
-                            The Apollo Group, profit before tax, increased by 9% to Ksh 947 million, reflecting improved operational performance.  The group registered a 14% increase in premiums from Ksh 9.6 billion to Ksh 11.0 billion and grew its asset base from Ksh 23 billion in 2017 to Ksh 24 billion in 2018.
-
-                            APA Insurance grew by 15% reporting significant growth in it’s Gross Written Premiums (GWP) in 2018 to Ksh 9.56 Billion from Ksh 8.30 billion in 2017.  This is a 15% year on year growth against the insurance industry estimated growth of 4% and now makes APA the third largest General Insurer in Kenya.   APA posted KSh 677 million in profit before tax.  The total claims settled by the APA Insurance during the year were KSh6.54 billion compared to KSh 6.38 billion in 2017.
+                            The Apollo Group, profit before tax, increased by 9% to Ksh 947 million, reflecting improved operational performance.  
+                            The group registered a 14% increase in premiums from Ksh 9.6 billion to Ksh 11.0 billion and grew its asset base from Ksh 23 billion in 2017 to Ksh 24 billion in 2018.
+                            <br><br>
+                            APA Insurance grew by 15% reporting significant growth in it’s Gross Written Premiums (GWP) in 2018 to Ksh 9.56 Billion from Ksh 8.30 billion in 2017. 
+                            This is a 15% year on year growth against the insurance industry estimated growth of 4% and now makes APA the third largest General Insurer in Kenya.   APA posted KSh 677 million in profit before tax. 
+                            The total claims settled by the APA Insurance during the year were KSh6.54 billion compared to KSh 6.38 billion in 2017.
+                            <br><br>
 
                             APA Life Assurance has declared 10.25% to pension contributors which is one of the highest in the industry.
-
-                            Commenting on the results, Apollo Group CEO Ashok Shah said: “In 2018 the Group has delivered concrete results against the backdrop of a difficult business environment.  We have achieved a growth in premiums and in solid investment returns, when many of our peers have shown losses in both operational and investment returns”
-
-                            Mr Shah added: “APA Insurance is still the flagship and continues to grow and provides healthy dividends to the group with very strong ratios.   The Group will continue to focus on growing APA Life Assurance and expanding the APA Insurance’s general business in Uganda.  This indeed is a testimony of our commitment over the long haul in this market.  It is our vision to continue being at the forefront in insurance and investments in Kenya and other chosen markets.  The Board and Management have agreed that the refreshed strategy of Insuring Happiness, with a greater focus on customer centricity, will position the company for further significant growth.”
-
+                            <br><br>
+                            Commenting on the results, Apollo Group CEO Ashok Shah said: “In 2018 the Group has delivered concrete results against the backdrop of a difficult business environment.  We have achieved a growth in 
+                            premiums and in solid investment returns, when many of our peers have shown losses in both operational and investment returns”
+                            <br><br>
+                            Mr Shah added: “APA Insurance is still the flagship and continues to grow and provides healthy dividends to the group with very strong ratios.
+                            The Group will continue to focus on growing APA Life Assurance and expanding the APA Insurance’s general business in Uganda.  This indeed is a testimony of our commitment over the long haul in this market.
+                            It is our vision to continue being at the forefront in insurance and investments in Kenya and other chosen markets.  The Board and Management have agreed that the refreshed strategy of Insuring Happiness, 
+                            with a greater focus on customer centricity, will position the company for further significant growth.”
+                            <br><br>
                             Following the impressive results, APA has declared a Ksh 600 million dividend to its shareholders.    
                         </p2>
                         <hr>
