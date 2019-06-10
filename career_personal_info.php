848a23d6703ebaecfca5744f84826c734dc6d4fc
kangethu2 <kangethu2@gmail.com>
2019-06-10 11:40:59 +0300

    updated files

---
+++ career_personal_info.php
@@ -0,0 +1,226 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>APA INSURANCE</title>
+
+    <?php include 'views/head_links.php'; ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="js/submit_personal_info.js"></script> <!-- load our javascript file -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <!-- My js copy -->
+    <script type="text/javascript" src="js/workexperience.js"></script>
+    <script type="text/javascript" src="js/change.js"></script>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+   <!-- <script src="js/submit.js"></script> -->
+
+
+
+</head>
+
+<body>
+
+
+    <!-- ==============HEADER=============== -->
+    <?php include 'views/career_portal_nav.php'; ?>
+    <!-- #header -->
+
+    <!-- ===================================== JOB DESCRIPTION ===================================== -->
+    <div id="msform">
+
+        <ul id="progressbar" class="nav-list">
+            <li class="act act" id="active1">PERSONAL <br>INFORMATION</li>
+            <li class="act" id="active2">EMPLOYMENT<br> HISTORY</li>
+            <li class="act" id="active3">EDUCATION<br> BACKGROUND</li>
+            <li class="act" id="active4">FILE <br>ATTACHMENT</li>
+            <li class="act" id="active5">REVIEW <br>AND SUBMIT</li>
+            <!-- <li ><br>SUMMARY</li> -->
+        </ul>
+
+
+        <!-- PERSONAL INFORMATION FORM -->
+
+       <fieldset id="first">
+            <div class="container">
+                <br>
+                <div class="row">
+                    <div class="col-10 job-box">
+                        <div class="job-description text-center">
+                            <h2 id="personal">PERSONAL INFORMATION</h2>
+                            <p>Please enter all relevant personal information in the fields below</p>
+                            <form  action="submit_personal_info.php" method="POST">
+                                <br>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label id="label-1" for="exampleInputEmail1"><span
+                                                    class="icon_star">&#10040;</span>First Name</label>
+                                                    <input type="text" class="form-control" name="first_name" placeholder="Name"></div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1"><span
+                                                    class="icon_star">&#10040;</span>Surname</label>
+                                            <input type="text" name="surname" class="form-control my-input"
+                                                placeholder="">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1"><span class="icon_star">&#10040;</span>Other
+                                                Name</label>
+                                            <input type="text" name="other_names" class="form-control my-input"
+                                                placeholder="">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label id="label-1" for="exampleInputEmail1"><span
+                                                    class="icon_star">&#10040;</span>National ID</label>
+                                            <input type="text" class="form-control my-input" name="id_no"
+                                                placeholder="">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1"><span class="icon_star">&#10040;</span>
+                                                Gender</label>
+                                            <select class="form-control my-input" name="gender">
+                                                <option selected="selected" disabled>--Please choose --</option>
+                                                <option value="MALE">MALE</option>
+                                                <option value="FEMALE">FEMALE</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1"><span class="icon_star">&#10040;</span>Date
+                                                of Birth</label>
+                                            <input type="date" name="dob" class="form-control my-input" placeholder="">
+                                        </div>
+                                    </div>
+                                </div>
+                        
+
+                        </br>
+
+                       
+
+                                <hr>
+
+                                <h3>PLACE OF RESIDENCE</h3>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label id="label-1" for="exampleInputEmail1"><span
+                                                    class="icon_star">&#10040;</span>COUNTRY</label>
+                                            <input type="text" name="country" class="form-control my-input"
+                                                aria-describedby="basic-addon" placeholder="">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1"><span
+                                                    class="icon_star">&#10040;</span>STATE/PROVINCE/COUNTY</label>
+                                            <input type="text" name="state" class="form-control my-input"
+                                                placeholder="">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1"><span
+                                                    class="icon_star">&#10040;</span>REGION/CITY</label>
+                                            <input type="text" name="city" class="form-control my-input" placeholder="">
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr>
+                                <h3>CONTACT DETAILS</h3>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1"><span class="icon_star">&#10040;</span>Phone
+                                        Number</label>
+                                    <input type="tel" name="phone" class="form-control my-input" placeholder="">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1"><span class="icon_star">&#10040;</span>Email
+                                        address</label>
+                                    <input type="email" name="email" class="form-control my-input"
+                                        id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
+                                </div>
+                                <!-- <div class="form-group">
+                                    <label for="exampleInputEmail1">EXPECTED SALARY</label>
+                                    <input type="text" name="salary" class="form-control my-input" placeholder="">
+                                </div> -->
+                                <br>
+                                <div class="row btn-form">
+                                     <div class="col-md-3">
+                                        <div class="text-center new-bt bt-back">
+                                            <a href="#" class="btn btn-secondary">BACK</a>
+                                        </div>
+                                    </div> 
+                                    <div class="col-md-6">
+                                        <div class="text-center  new-bt">
+                                            <!-- <a href="career_employment.html" class="btn btn-primary">save & continue</a> 
+                                             <input name="next" id="submitFormData" onclick="SubmitFormData();" class="btn btn-primary next" type="submit" value="save & continue"> 
+                                         <input type="button" id="submitFormData" onclick="SubmitFormData();" value="Submit" />  
+ 
+                                         <button type="submit" id="submitFormData" onclick="SubmitFormData();" class="btn btn-success">Submit <span class="fa fa-arrow-right"></span></button>
+                                      <button type="submit" class="btn btn-success">Submit <span class="fa fa-arrow-right"></span></button>-->
+                                       <input id="next_btn1" onclick="next_step1()" class="btn btn-primary next"
+                                                type="button" value="save & continue">
+
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class=" text-center new-bt bt-back">
+                                            <a href="#" class="btn btn-danger g-log1">QUIT</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                            </form>
+                             <div id="results">
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </fieldset>  
+
+
+
+    <!-- =====================================FOOTER===================================== -->
+    <?php include 'views/footer.php'; ?>
+    <!-- #footer -->
+    <!-- Optional JavaScript -->
+
+    <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+
+
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script src="js/summary.js" type="text/javascript"></script>
+</body>
+
+</html>
---
+++ submit_personal_info.php
@@ -0,0 +1,90 @@
+<?php
+
+$errors         = array();  	// array to hold validation errors
+$data 			= array(); 		// array to pass back data
+
+// validate the variables ======================================================
+	// if any of these variables don't exist, add an error to our $errors array
+
+	    if (empty($first_name=$_POST['first_name']))
+		   $errors['first_name'] = 'First name required.';
+
+		if (empty($surname=$_POST['surname']))
+		   $errors['surname'] = 'Surname required.';
+
+		if (empty($other_names=$_POST['other_names']))
+		   $errors['other_names'] = 'Other names required.';
+
+		if (empty($id_no=$_POST['id_no']))
+		   $errors['id_no'] = 'Nationa ID required.';
+
+		if (empty($gender=$_POST['gender']))
+		   $errors['gender'] = 'Gender required.';
+
+		if (empty($dob=$_POST['dob']))
+		   $errors['dob'] = 'Date of birth required.';
+
+		if (empty($country=$_POST['country']))
+		   $errors['country'] = 'Country required.';
+
+		if (empty($state=$_POST['state']))
+		   $errors['stete'] = 'State required.';
+
+		if (empty($city=$_POST['city']))
+		   $errors['city'] = 'City required.';
+
+		if (empty($phone=$_POST['phone']))
+		   $errors['phone'] = 'Phone required.';
+
+		if (empty($email=$_POST['email']))
+		   $errors['email'] = 'Email required.';
+
+
+
+
+
+// return a response ===========================================================
+
+	// if there are any errors in our errors array, return a success boolean of false
+	if ( ! empty($errors)) {
+
+		// if there are items in our errors array, return those errors
+		$data['success'] = false;
+		$data['errors']  = $errors;
+	} else {
+
+		// if there are no errors process our form, then return a message
+
+		// DO ALL YOUR FORM PROCESSING HERE
+		// THIS CAN BE WHATEVER YOU WANT TO DO (LOGIN, SAVE, UPDATE, WHATEVER)
+
+		// show a message of success and provide a true success variable
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "apa";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//Insert data in the DB
+$sql = "INSERT INTO apa_job_applicants_personal_info (first_name,surname,other_names,national_id,gender,dob,country,county,city,phone_number,email)
+VALUES ('$first_name','$surname','$other_names','$id_no','$gender','$dob','$country','$state','$city','$phone','$email')";
+if ($conn->query($sql) === TRUE) {
+    
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+		$data['success'] = true;
+		$data['message'] = 'Success!';
+	}
+
+	// return all our data to an AJAX call
+	echo json_encode($data);
