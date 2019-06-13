77511064188214643c07d39b6ed32e294db66cfa
peterkinuthia <peterchege442@gmail.com>
2019-06-13 16:08:37 +0300

    slider widget

--- about_us.php
+++ about_us.php
@@ -110,78 +110,98 @@
         <div class="product product-new text-left">
 
             <h1>OUR HISTORY</h1>
+            <div class="container-fluid ">
+            <div class="uk-position-relative uk-visible-toggle uk-light slide-history" tabindex="-1" uk-slider>
 
-            <div class="container-fluid">
-            <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider>
+        <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@m uk-grid">
+            <li>
+                <div class="uk-panel">
+                    <img src="images/socila2.jpg" alt="">
+                    <div class="uk-position-center uk-panel"><h1>1981</h1></div>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae, alias sequi
+                            sapiente atque, accusamus, error earum vitae 
+                    </p>
+                </div>
+            </li>
+            <li>
+                <div class="uk-panel">
+                    <img src="images/soc.jpg" alt="">
+                    <div class="uk-position-center uk-panel"><h1>1982</h1></div>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae, alias sequi
+                            sapiente atque, accusamus, error earum vitae 
+                    </p>
+                </div>
+            </li>
+            <li>
+                <div class="uk-panel">
+                    <img src="images/social1.jpg" alt="">
+                    <div class="uk-position-center uk-panel"><h1>1983</h1></div>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae, alias sequi
+                            sapiente atque, accusamus, error earum vitae 
+                    </p>
+                </div>
+            </li>
+            <li>
+                <div class="uk-panel">
+                    <img src="images/social4.jpg" alt="">
+                    <div class="uk-position-center uk-panel"><h1>1984</h1></div>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae, alias sequi
+                            sapiente atque, accusamus, error earum vitae 
+                    </p>
+                </div>
+            </li>
+            <li>
+                <div class="uk-panel">
+                    <img src="images/social3.jpg" alt="">
+                    <div class="uk-position-center uk-panel"><h1>1985</h1></div>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae, alias sequi
+                            sapiente atque, accusamus, error earum vitae 
+                    </p>
+                </div>
+            </li>
+            <li>
+                <div class="uk-panel">
+                    <img src="images/social1.jpg" alt="">
+                    <div class="uk-position-center uk-panel"><h1>2003</h1></div>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae, alias sequi
+                            sapiente atque, accusamus, error earum vitae 
+                    </p>
+                </div>
+            </li>
+            <li>
+                <div class="uk-panel">
+                    <img src="images/soc.jpg" alt="">
+                    <div class="uk-position-center uk-panel"><h1>2009</h1></div>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae, alias sequi
+                            sapiente atque, accusamus, error earum vitae 
+                    </p>
+                </div>
+            </li>
+            <li>
+                <div class="uk-panel">
+                    <img src="images/social1.jpg" alt="">
+                    <div class="uk-position-center uk-panel"><h1>2013</h1></div>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae, alias sequi
+                            sapiente atque, accusamus, error earum vitae 
+                    </p>
+                </div>
+            </li>
+        </ul>
 
-    <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@m uk-grid">
-        <li>
-            <div class="uk-panel">
-                <img src="images/slider1.jpg" alt="">
-                <div class="uk-position-center uk-panel"><h1>1</h1></div>
-            </div>
-        </li>
-        <li>
-            <div class="uk-panel">
-                <img src="images/slider2.jpg" alt="">
-                <div class="uk-position-center uk-panel"><h1>2</h1></div>
-            </div>
-        </li>
-        <li>
-            <div class="uk-panel">
-                <img src="images/slider3.jpg" alt="">
-                <div class="uk-position-center uk-panel"><h1>3</h1></div>
-            </div>
-        </li>
-        <li>
-            <div class="uk-panel">
-                <img src="images/slider4.jpg" alt="">
-                <div class="uk-position-center uk-panel"><h1>4</h1></div>
-            </div>
-        </li>
-        <li>
-            <div class="uk-panel">
-                <img src="images/slider5.jpg" alt="">
-                <div class="uk-position-center uk-panel"><h1>5</h1></div>
-            </div>
-        </li>
-        <li>
-            <div class="uk-panel">
-                <img src="images/slider1.jpg" alt="">
-                <div class="uk-position-center uk-panel"><h1>6</h1></div>
-            </div>
-        </li>
-        <li>
-            <div class="uk-panel">
-                <img src="images/slider2.jpg" alt="">
-                <div class="uk-position-center uk-panel"><h1>7</h1></div>
-            </div>
-        </li>
-        <li>
-            <div class="uk-panel">
-                <img src="images/slider3.jpg" alt="">
-                <div class="uk-position-center uk-panel"><h1>8</h1></div>
-            </div>
-        </li>
-        <li>
-            <div class="uk-panel">
-                <img src="images/slider4.jpg" alt="">
-                <div class="uk-position-center uk-panel"><h1>9</h1></div>
-            </div>
-        </li>
-        <li>
-            <div class="uk-panel">
-                <img src="images/slider5.jpg" alt="">
-                <div class="uk-position-center uk-panel"><h1>10</h1></div>
-            </div>
-        </li>
-    </ul>
-
-    <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
-    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
 
 </div>
 
+<br><br>
             </div>
 
 
