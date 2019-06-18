536544120e902942bc06ccbb15897eed4560be19
peterkinuthia <peterchege442@gmail.com>
2019-06-18 12:05:27 +0300

    company pages with content

--- companies.php
+++ companies.php
@@ -51,28 +51,86 @@
                 <a class="nav-link" href="#">DOWNLOADS</a>
             </li>
         </ul>
-    </div>    
+    </div>
 
 
     <!-- ===================================== ABOUT US ===================================== -->
 
 
-    <div class="container">
-        <div class="apollo">
-            <h1>WELCOME TO APOLLO GROUP</h1>
-            <div class="under-line img5">
+    <div class="container-fluid12">
+        <div class="container apollo">
+            <h1 class="wow fadeInUp" data-wow-delay="0.1s">OUR COMPANIES</h1>
+            <div class="under-line img7">
                 <img src="images/line.png" alt="">
             </div>
 
-            <p class="container content-offer wow fadeInUp" data-wow-delay="0.1s">Welcome to Apollo Investments Limited.
-                Since our founding in 1977, our mission has been to provide
-                present and future peace of mind to the East African market and, in turn, enhance the quality of life
-                for both our clients and their loved ones.Built on commitment, integrity and innovation, we have risen
-                to become one of the leading Financial Service Groups in East Africa. The Apollo Group has 6 companies;
-                APA Insurance, APA Life, APA Uganda, Apollo Asset Management, Gordon Court (Apollo Centre) and Reliance
-                Insurance, Tanzania.
+            <p class="container content-offer wow fadeInUp" data-wow-delay="0.1s">
+                Welcome to Apollo Investments Limited. The flagship company, APA Insurance, was formed
+                after the merger of the general businesses of Apollo Insurance Company and Pan Africa Insurance.
+                With a combined experience of over 100 years (Pan Africa – 1946 and Apollo –1977), APA has become
+                the largest insurance firm in Kenya and the most innovative in product offering. APA Insurance
+                underwrites General Insurance risks such as Motor, Agriculture, Marine, and Micro Insurance.
+                We also underwrite Individual and Family Health Insurance. Our products provide customers with peace
+                of mind, helping both businesses and individuals to return to the same standing following a loss or
+                other unfortunate event.
             </p>
         </div>
+
+        <div class="company-box apollo">
+            <h1 class="wow fadeInUp" data-wow-delay="0.1s">APA INSURANCE (UGANDA) LIMITED </h1>
+            <div class="under-line img9">
+                <img src="images/line.png" alt="">
+            </div>
+
+            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                Our wholly owned subsidiary underwrites General and Health Insurance .The firms market share and ranking
+                has improved and has been recognised for Best Claims Service by the insurance industry in Uganda.
+            </p>
+        </div>
+
+
+        <div class="apollo">
+            <h1 class="wow fadeInUp" data-wow-delay="0.1s">APA LIFE ASSURANCE LIMITTED </h1>
+            <div class="under-line img10">
+                <img src="images/line.png" alt="">
+            </div>
+
+            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                APA Life is one of the leading insurance companies in Kenya, providing Life Insurance and Pension Benefits.
+                It has been recognised as the Best Insurance Company in Claims Settlement (Life Business). Our products
+                include: Employee Benefits such as Group Life Assurance, Last Expense, Disability Covers and Individual
+                Pension Plans. We also provide Unit Linked Investment Plans and Education Plans, among many others.
+            </p>
+        </div>
+
+        <div class="company-box  apollo">
+            <h1 class="wow fadeInUp" data-wow-delay="0.1s">APOLLO ASSET MANAGMENT </h1>
+            <div class="under-line img1">
+                <img src="images/line.png" alt="">
+            </div>
+
+            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                Apollo Asset Management Company (AAMC) is focused exclusively on meeting needs of institutional investors
+                like pension schemes, Saccos, and High Net Worth investors and their families. AAMC is licensed by the Capital
+                Markets Authority (CMA) and the Retirement Benefits Authority (RBA) to conduct Fund Management and Investment
+                Advisory Services.
+            </p>
+        </div>
+
+        <div class="apollo">
+            <h1 class="wow fadeInUp" data-wow-delay="0.1s">APOLLO CENTRE</h1>
+            <div class="under-line img2">
+                <img src="images/line.png" alt="">
+            </div>
+
+            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                Is wholly owned by Gordon Court Limited, a subsidiary of AIL. It is a commercial building situated in 
+                the Westlands area of Nairobi. Besides being the group headquarters, Apollo Centre offers modern conference
+                facilities including: private meeting rooms and custom designed training rooms fitted with state-of-the-art equipment. 
+                The facility is popular with a number of corporate and individual clients due to its exclusivity and convenience.
+            </p>
+        </div>
+
     </div>
 
     <!-- =================================== WE DO MORE ===================================== -->
