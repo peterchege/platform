36043f033920cc5177637e76155059a496e902e7
anthony-baru <anthonybaru@gmail.com>
2019-06-18 10:24:25 +0300

    modified init.php linkedin

--- linkedin2/init.php
+++ linkedin2/init.php
@@ -11,5 +11,5 @@ $app_secret = 'ViOaJis9SrZ7P6hs';
 $app_callback = 'http://localhost/apainsurance/linkedin2/callback.php';
 $app_scopes = 'r_basicprofile r_emailaddress w_member_social rw_company_admin w_share r_liteprofile';
 
-$ssl = false; //always true when we go live
+$ssl = false; //always true when site goes live
 $linkedin = new LinkedIn($app_id, $app_secret, $app_callback, $app_scopes, $ssl);
