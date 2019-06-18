df893b05d51dbb226cec1975f175ef19756a5111
anthony-baru <anthonybaru@gmail.com>
2019-06-18 14:41:20 +0300

    modified companies.php

--- companies.php
+++ companies.php
@@ -45,7 +45,7 @@
                 <a class="nav-link" href="#">LEADERSHIP</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">MANAGMENT</a>
+                <a class="nav-link" href="#">MANAGEMENT</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">DOWNLOADS</a>
