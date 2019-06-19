fd9a72cb6e4f3ba6b81138a4723dc282329ae055
peterkinuthia <peterchege442@gmail.com>
2019-06-19 20:22:06 +0300

    correct image

--- leadership.php
+++ leadership.php
@@ -300,7 +300,7 @@
         <div class="uk-modal-dialog">
             <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
             <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
-                <div class="uk-background-cover" style="background-image: url('img/board/mary.jpg');" uk-height-viewport></div>
+                <div class="uk-background-cover" style="background-image: url('img/board/richard.jpg');" uk-height-viewport></div>
                 <div class="uk-padding-large">
                     <h1>
                         Richard Ashley
