9eee8af65d465419eed64a199e7fa5bf2c3dfae4
peterkinuthia <peterchege442@gmail.com>
2019-06-19 21:04:02 +0300

    Piyush Shah

--- leadership.php
+++ leadership.php
@@ -392,6 +392,30 @@
         </div>
     </div>
 
+    <!-- =====================================  Piyush Shah ===================================== -->
+
+    <div id="modal-full7" class="uk-modal-full" uk-modal>
+        <div class="uk-modal-dialog">
+            <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
+            <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
+                <div class="uk-background-cover" style="background-image: url('img/board/piyush.jpg');" uk-height-viewport></div>
+                <div class="uk-padding-large">
+                    <h1>
+                        Piyush Shah
+                    </h1>
+                    <h2 class="small-head">
+                        Director
+                    </h2>
+                    <p>Piyush Shah is a Chartered Accountant with a background in IT enterprises with over 30 years’ experience in
+                        both these fields. He is an active angel investor and is a non-executive director of different companies.
+                        Piyush draws his experience from firms both in Kenya and in the United Kingdom
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- =====================================  Reto Schnarwiler ===================================== -->
 
     <div id="modal-full8" class="uk-modal-full" uk-modal>
