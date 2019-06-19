9d7a00638645801c0b35ce221bba588f357d70bf
peterkinuthia <peterchege442@gmail.com>
2019-06-19 10:14:03 +0300

    work plan

---
+++ managment_asset.php
@@ -0,0 +1,276 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>APA INSURANCE</title>
+    <link rel="stylesheet" href="css/companies.css" media="screen">
+
+    <?php include 'views/head_links.php'; ?>
+
+</head>
+
+<body>
+
+    <!--==========================
+    Header
+    ============================-->
+    <?php include 'views/nav.php'; ?>
+    <!-- #header -->
+
+    <!--==========================
+    Intro Section
+    ============================-->
+    <div class="banner-cover">
+
+    </div>
+    <div class="cover-line">
+        <img src="images/line.png" alt="">
+    </div>
+
+
+    <!-- #intro -->
+
+    <!-- ===================================== CHILD MENU BAR ===================================== -->
+
+    <div class="company-nav">
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link" href="companies.php">COMPANIES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="leadership.php">LEADERSHIP</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="managment.php">MANAGMENT</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">DOWNLOADS</a>
+            </li>
+        </ul>
+    </div>
+
+
+
+    <!-- ===================================== ABOUT US ===================================== -->
+
+
+    <div class="container-fluid12">
+        <div class="container apollo">
+            <h1 class="wow fadeInUp" data-wow-delay="0.1s">APA LIFE MANAGMENT </h1>
+            <div class="under-line img6">
+                <img src="images/line.png" alt="">
+            </div>
+
+            <p class="container content-offer wow fadeInUp" data-wow-delay="0.1s">
+                Welcome to Apollo Investments Limited. The flagship company, APA Insurance, was formed
+                after the merger of the general businesses of Apollo Insurance Company and Pan Africa Insurance.
+                With a combined experience of over 100 years.
+            </p>
+        </div>
+
+        <!-- APA INSURANCE -->
+        <div class="company-box apollo">
+            <div class="container">
+
+                <div class="row lead-img">
+
+                    <div class="col-md-3 leader-img wow fadeInUp" data-wow-delay="0.1s">
+                        <img src="img/managment/fred.jpg" class="img" alt="">
+                        <h5 class="title-name">
+                            Fred Mburu<br>
+                            <small>Chief Executive Officer</small>
+                        </h5>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                        <button class="btn btn-primary">Read more</button>
+                    </div>
+
+                    <div class="col-md-3 leader-img wow fadeInUp" data-wow-delay="0.1s">
+                        <img src="img/managment/mugo.jpg" class="img" alt="">
+                        <h5 class="title-name">
+                            Amos Kosgei<br>
+                            <small>Portfolio Manager</small>
+                        </h5>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                        <button class="btn btn-primary">Read more</button>
+                    </div>
+
+                    <div class="col-md-3 leader-img wow fadeInUp" data-wow-delay="0.1s">
+                        <img src="img/managment/solo.jpg" class="img" alt="">
+                        <h5 class="title-name">
+                            Solomon Watiki<br>
+                            <small>Head of Operations</small>
+                        </h5>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                        <button class="btn btn-primary">Read more</button>
+                    </div>
+
+                    <div class="col-md-3 leader-img wow fadeInUp" data-wow-delay="0.1s">
+                        <img src="img/managment/eva.jpg" class="img" alt="">
+                        <h5 class="title-name">
+                            Everlyne Kinyanjui<br>
+                            <small>Head of Corporate Business</small>
+                        </h5>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                        <button class="btn btn-primary">Read more</button>
+                    </div>
+
+                </div>
+
+                <div class="row lead-img">
+
+                    <div class="col-md-3 leader-img wow fadeInUp" data-wow-delay="0.1s">
+                        <img src="img/managment/stevo.jpg" class="img" alt="">
+                        <h5 class="title-name">
+                            Stephen Muiga<br>
+                            <small>Business Development Manager</small>
+                        </h5>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                        <button class="btn btn-primary">Read more</button>
+                    </div>
+
+                    <div class="col-md-3 leader-img wow fadeInUp" data-wow-delay="0.1s">
+                        <img src=" img/managment/mark.jpg" class="img" alt="">
+                        <h5 class="title-name">
+                            Mark Mumo<br>
+                            <small>Business Development Manager</small>
+                        </h5>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                        <button class="btn btn-primary">Read more</button>
+                    </div>
+
+                    <div class="col-md-3 leader-img wow fadeInUp" data-wow-delay="0.1s">
+                        <img src="img/managment/njagi.jpg" class="img" alt="">
+                        <h5 class="title-name">
+                            James Njagi<br>
+                            <small>Business Development Manager</small>
+                        </h5>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                        <button class="btn btn-primary">Read more</button>
+                    </div>
+
+                    <div class="col-md-3 leader-img wow fadeInUp" data-wow-delay="0.1s">
+                        <img src="img/managment/amos.jpg" class="img" alt="">
+                        <h5 class="title-name">
+                            Bernadicto Makena<br>
+                            <small>National Sales Manager</small>
+                        </h5>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                        <button class="btn btn-primary">Read more</button>
+                    </div>
+
+
+                </div>
+
+
+
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+
+
+    <!-- =====================================FOOTER===================================== -->
+    <?php include 'views/footer.php'; ?>
+    <!-- #footer -->
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+
+    <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+
+
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="lib/jquery/jquery.min.js"></script>
+    <script src="lib/jquery/jquery-migrate.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/superfish/hoverIntent.js"></script>
+    <script src="lib/superfish/superfish.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+    <script src="lib/touchSwipe/jquery.touchSwipe.min.js"></script>
+
+    <!-- Template Main Javascript File -->
+    <script src="js/main.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
