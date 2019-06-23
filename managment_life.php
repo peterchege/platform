6dc694403791f93d162139989e8a17092aa27ea0
peterkinuthia <peterchege442@gmail.com>
2019-06-23 20:14:11 +0300

    Vitalis Mbae correct

--- managment_life.php
+++ managment_life.php
@@ -488,7 +488,7 @@
         <div class="uk-modal-dialog">
             <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
             <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
-                <div class="uk-background-cover" style="background-image: url('img/managment/makena.jpg');" uk-height-viewport></div>
+                <div class="uk-background-cover" style="background-image: url('img/managment/ruto.jpg');" uk-height-viewport></div>
                 <div class="uk-padding-large">
                     <h1>
                         Vitalis Mbae
