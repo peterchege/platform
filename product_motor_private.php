be350971d1c2cd9855419f3da66cbdb8bce5ca1d
peterkinuthia <peterchege442@gmail.com>
2019-06-26 21:05:31 +0300

    LEAD FORM FOR COMPREHENSIVE

--- product_motor_private.php
+++ product_motor_private.php
@@ -9,6 +9,7 @@
     <title>APA INSURANCE</title>
     <link rel="stylesheet" href="css/companies.css" media="screen">
     <link rel="stylesheet" href="css/product.css" media="screen">
+    <link rel="stylesheet" href="css/apollo_centre.css" media="screen">
 
     <?php include 'views/head_links.php'; ?>
 
@@ -95,9 +96,9 @@
 
             <div class="row btn-comprehensive">
                 <div class="col-12  text-center">
-                    <a href="#" class="btn btn-primary">
+                    <button class="btn btn-primary" data-target="#modal-full" uk-toggle>
                         Get cover
-                    </a>
+                    </button>
                 </div>
             </div>
 
@@ -111,6 +112,93 @@
         <?php include 'views/footer.php'; ?>
         <!-- #footer -->
 
+        <!-- ===================================== LEAD FORM FOR COMPREHENSIVE ===================================== -->
+
+        <div id="modal-full" class="uk-modal-full" uk-modal>
+            <div class="uk-modal-dialog">
+                <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
+                <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-top" uk-grid>
+                    <div class="uk-background-cover" style="background-image: url('images/auto.jpg'); background-size: cover;" uk-height-viewport></div>
+                    <div class="uk-padding-large">
+
+
+                        <form class="form-container">
+                            <h3 for="inputAddress" class="comp-detail">PERSONAL DETAIL</h3>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="inputAddress">Name</label>
+                                        <input name="company_name" type="text" class="form-control" id="inputAddress" placeholder="Full Name" value="<?= ((isset($company_name)) ? $company_name : '') ?>">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="inputAddress2">Phone Number</label>
+                                        <input name="phone" type="tel" class="form-control" id="inputAddress2" placeholder="Phone Number" value="<?= ((isset($phone)) ? $phone : '') ?>">
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="inputAddress">Email Address</label>
+                                        <input name="email" type="email" class="form-control" id="inputAddress" placeholder="Email" value="<?= ((isset($email)) ? $email : '') ?>">
+                                    </div>
+                                    <div class=" form-group col-md-6">
+                                        <label for="inputAddress2">Capacity</label>
+                                        <input name="capacity" type="number" class="form-control" id="inputAddress2" placeholder="pax" value="<?= ((isset($capacity)) ? $capacity : '') ?>">
+                                    </div>
+                                </div>
+
+                            </div>
+                            <hr>
+                            <div class="form-group col-md-12">
+                                <h3 for="inputAddress">VIHECLE DETAILS</h3>
+                            </div>
+
+                            <div class="container">
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="inputAddress">Vehicle Type</label>
+                                        <input name="company_name" type="text" class="form-control" id="inputAddress" placeholder="station wagon" value="<?= ((isset($company_name)) ? $company_name : '') ?>">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="inputAddress2">Model Number</label>
+                                        <input name="text" type="tel" class="form-control" id="inputAddress2" placeholder="crown" value="<?= ((isset($phone)) ? $phone : '') ?>">
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="inputAddress">Vehicle Value (Kshs)</label>
+                                        <input name="number" type="Vehicle_value" class="form-control" id="inputAddress" placeholder="1,400,000" value="<?= ((isset($email)) ? $email : '') ?>">
+                                    </div>
+                                    <div class=" form-group col-md-6">
+                                        <label for="inputAddress2">Capacity</label>
+                                        <input name="capacity" type="number" class="form-control" id="inputAddress2" placeholder="pax" value="<?= ((isset($capacity)) ? $capacity : '') ?>">
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group col-md-12">
+                                        <label for="inputAddress">More Information</label>
+                                        <textarea name="more_information" class="form-control"></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="row book-btn">
+                                    <div class="col-md-12">
+                                        <button class="btn btn-primary">REQUEST QUOTE</button>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <!-- Optional JavaScript -->
 
