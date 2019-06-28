bf76fe50a0161fe10b03ec5de93998827de8b705
peterkinuthia <peterchege442@gmail.com>
2019-06-28 10:39:37 +0300

    product_details_motor

---
+++ product_detail_motor.php
@@ -0,0 +1,191 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>APA INSURANCE</title>
+    <link rel="stylesheet" href="css/product.css" />
+    <link rel="stylesheet" href="css/career.css" />
+    <?php include 'views/head_links.php'; ?>
+
+</head>
+
+<body>
+
+
+    <!-- ==============HEADER=============== -->
+    <?php include 'views/nav.php'; ?>
+    <!-- #header -->
+
+
+    <!-- ===================================== JOB DESCRIPTION ===================================== -->
+    <div class="container-fluid212">
+        <div class="header-image header-image1a">
+        </div>
+    </div>
+
+    <div class="container product-container">
+
+
+
+        <div class="row">
+
+            <div class="col-10 job-box1">
+
+
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">OVERVIEW</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">FEATURES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">BENEFITS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">HOW DO I START</a>
+                    </li>
+
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+
+
+
+                    </div>
+                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+                </div>
+
+                <br>
+
+                <div class="job-description text-center">
+                    <h2>PRIVATE MOTOR COVER</h2>
+                    <P class="text-justify">Living a healthy stress free life is every persons dream. A healthy family
+                        is a
+                        happy family.
+                        This is why we have medical insurance products designed to cover individuals and families.
+                        Our main products include:
+                        Inpatient cover is the core cover.
+                        With this, one is covered for; Pre-existing, Chronic, Congenital, HIV/AIDS related conditions,
+                        cancer treatment, organ transplant, lodger fees, accidental dental/reconstructive surgery among
+                        many
+                        more others.</P>
+                    <hr>
+                    <div class="job-content">
+                        <div class="text-justify">
+                            <h2>Our main products include:</h2>
+                            <p> Inpatient cover is the core cover.</p>
+                            <p2>With this, one is covered for; Pre-existing, Chronic, Congenital, HIV/AIDS related
+                                conditions, cancer treatment, organ transplant, lodger fees, accidental
+                                dental/reconstructive surgery among many more others.</p2>
+                            <p>Optional Benefits:</p>
+                            <ul>
+                                <li>Outpatient cover </li>
+                                <li>Maternity cover </li>
+                                <li>Dental & Optical Cover </li>
+                                <li>Last expense cover </li>
+                                <li>Personal Accident Benefit Cover</li>
+                            </ul>
+                            <hr>
+                            <h3>The cover also provides for enhanced benefits. This include:</h3>
+                            <ul>
+                                <li>Cover for Pre-Existing, Chronic, Psychiatric, congenital and HIV/AIDS including
+                                    related
+                                    conditions
+                                </li>
+                                <li>Country wide Provider Network
+                                </li>
+                                <li>Overseas inpatient referrals covered on credit under listed hospitals
+                                </li>
+                                <li>Cover for inpatient dental and optical treatment
+                                </li>
+                                <li>Post hospitalization benefit
+                                </li>
+                                <li>Funeral expenses benefit
+                                </li>
+                                <li>Free Personal Accident benefit for Principle member
+                                </li>
+                            </ul>
+
+                            <hr>
+                            <h3>Who is eligible for the cover?</h3>
+                            <ul>
+                                <li>Adults between the age of 18 years and 75 years.
+                                </li>
+                                <li>Children between the age of 1 month and 17 years.
+                                </li>
+
+                            </ul><br>
+                            <hr>
+                            <h3>Maternity Cover</h3>
+                            <p2>Children bring us happiness and blessings. As we prepared for our new bundle of joy, it
+                                is
+                                best that we cater for the mother’s health and conditions pre birth. This is why we have
+                                a
+                                Maternity Cover that caters for:</p2>
+                            <ul>
+                                <li>Normal delivery including professional fees, labour and recovery wards for all
+                                    plans.
+                                </li>
+                                <li>Costs of delivery, and other related ailments and complications including ectopic
+                                    pregnancies, miscarriage, etc.
+                                </li>
+                                <li>Caesarean section delivery including professional fees, labour and recovery wards.
+                                </li>
+
+                            </ul><br>
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="load-more text-center ">
+                                        <a href="#" class="btn btn-primary">Get Cover</a>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="load-more text-center ">
+                                        <a href="#" class="btn btn-danger"> Download Sample Policy </a>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="load-more text-center ">
+                                        <a href="#" class="btn btn-dark">I Need Assistance</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div><BR>
+            </div>
+        </div>
+    </div>
+
+    <!-- =====================================FOOTER===================================== -->
+    <?php include 'views/footer.php'; ?>
+    <!-- #footer -->
+
+
+    <!-- Optional JavaScript -->
+
+    <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+
+
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
