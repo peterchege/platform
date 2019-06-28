7cd7faf20a80a9166ac327b1ffb84deddd422353
peterkinuthia <peterchege442@gmail.com>
2019-06-28 14:45:28 +0300

    product detail jamii upgrades

--- product_detail_jamii.php
+++ product_detail_jamii.php
@@ -43,22 +43,283 @@
                             <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">BENEFITS</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">HOW DO I START</a>
+                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#start" role="tab" aria-controls="profile" aria-selected="false">HOW DO I START</a>
                         </li>
 
                     </ul>
                     <div class="tab-content" id="myTabContent">
+
+                        <!-- OVERVIEW -->
                         <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
 
+                            <div class="job-description text-center">
+
+                                <h2>JAMII PLUS </h2>
+                                <P class="text-justify">Living a healthy stress free life is every persons dream. A healthy family
+                                    is a
+                                    happy family.
+                                    This is why we have medical insurance products designed to cover individuals and families.
+                                    Our main products include:
+                                    Inpatient cover is the core cover.
+                                    With this, one is covered for; Pre-existing, Chronic, Congenital, HIV/AIDS related conditions,
+                                    cancer treatment, organ transplant, lodger fees, accidental dental/reconstructive surgery among
+                                    many
+                                    more others.</P>
+                                <hr>
+                                <div class="job-content">
+                                    <div class="text-justify">
+                                        <h2>Our main products include:</h2>
+                                        <p> Inpatient cover is the core cover.</p>
+                                        <p2>With this, one is covered for; Pre-existing, Chronic, Congenital, HIV/AIDS related
+                                            conditions, cancer treatment, organ transplant, lodger fees, accidental
+                                            dental/reconstructive surgery among many more others.</p2>
+                                        <p>Optional Benefits:</p>
+                                        <ol>
+                                            <li>Outpatient cover </li>
+                                            <li>Maternity cover </li>
+                                            <li>Dental & Optical Cover </li>
+                                            <li>Last expense cover </li>
+                                            <li>Personal Accident Benefit Cover</li>
+                                        </ol>
+                                        <hr>
+                                        <h3>The cover also provides for enhanced benefits. This include:</h3>
+                                        <ol>
+                                            <li>Cover for Pre-Existing, Chronic, Psychiatric, congenital and HIV/AIDS including
+                                                related
+                                                conditions
+                                            </li>
+                                            <li>Country wide Provider Network
+                                            </li>
+                                            <li>Overseas inpatient referrals covered on credit under listed hospitals
+                                            </li>
+                                            <li>Cover for inpatient dental and optical treatment
+                                            </li>
+                                            <li>Post hospitalization benefit
+                                            </li>
+                                            <li>Funeral expenses benefit
+                                            </li>
+                                            <li>Free Personal Accident benefit for Principle member
+                                            </li>
+                                        </ol>
+
+                                        <hr>
+                                        <h3>Who is eligible for the cover?</h3>
+                                        <ol>
+                                            <li>Adults between the age of 18 years and 75 years.
+                                            </li>
+                                            <li>Children between the age of 1 month and 17 years.
+                                            </li>
+
+                                        </ol><br>
+                                        <hr>
+                                        <h3>Maternity Cover</h3>
+                                        <p2>Children bring us happiness and blessings. As we prepared for our new bundle of joy, it
+                                            is
+                                            best that we cater for the mother’s health and conditions pre birth. This is why we have
+                                            a
+                                            Maternity Cover that caters for:</p2>
+                                        <ol>
+                                            <li>Normal delivery including professional fees, labour and recovery wards for all
+                                                plans.
+                                            </li>
+                                            <li>Costs of delivery, and other related ailments and complications including ectopic
+                                                pregnancies, miscarriage, etc.
+                                            </li>
+                                            <li>Caesarean section delivery including professional fees, labour and recovery wards.
+                                            </li>
+
+                                        </ol><br>
+                                        <div class="row">
+                                            <div class="col-4">
+                                                <div class="load-more text-center ">
+                                                    <a href="#" class="btn btn-primary">Get Cover</a>
+                                                </div>
+                                            </div>
+                                            <div class="col-4">
+                                                <div class="load-more text-center ">
+                                                    <a href="#" class="btn btn-danger"> Download Sample Policy </a>
+                                                </div>
+                                            </div>
+                                            <div class="col-4">
+                                                <div class="load-more text-center ">
+                                                    <a href="#" class="btn btn-dark">I Need Assistance</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <!-- FEATURES -->
+                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+
+                            <div class="job-description text-center">
+                                <div class="job-content">
+                                    <div class="text-justify">
+                                        <h2 class="text-center">Our main products include:</h2><br>
+                                        <p> Inpatient cover is the core cover.</p>
+                                        <p2>With this, one is covered for; Pre-existing, Chronic, Congenital, HIV/AIDS related
+                                            conditions, cancer treatment, organ transplant, lodger fees, accidental
+                                            dental/reconstructive surgery among many more others.</p2>
+                                        <p>Optional Benefits:</p>
+                                        <ol>
+                                            <li>Outpatient cover </li>
+                                            <li>Maternity cover </li>
+                                            <li>Dental & Optical Cover </li>
+                                            <li>Last expense cover </li>
+                                            <li>Personal Accident Benefit Cover</li>
+                                        </ol>
+                                        <hr>
+                                        <h3>The cover also provides for enhanced benefits. This include:</h3>
+                                        <ol>
+                                            <li>Cover for Pre-Existing, Chronic, Psychiatric, congenital and HIV/AIDS including
+                                                related
+                                                conditions
+                                            </li>
+                                            <li>Country wide Provider Network
+                                            </li>
+                                            <li>Overseas inpatient referrals covered on credit under listed hospitals
+                                            </li>
+                                            <li>Cover for inpatient dental and optical treatment
+                                            </li>
+                                            <li>Post hospitalization benefit
+                                            </li>
+                                            <li>Funeral expenses benefit
+                                            </li>
+                                            <li>Free Personal Accident benefit for Principle member
+                                            </li>
+                                        </ol>
+
+                                        <hr>
+                                        <h3>Who is eligible for the cover?</h3>
+                                        <ol>
+                                            <li>Adults between the age of 18 years and 75 years.
+                                            </li>
+                                            <li>Children between the age of 1 month and 17 years.
+                                            </li>
+
+                                        </ol><br>
+                                        <hr>
+                                        <h3>Maternity Cover</h3>
+                                        <p2>Children bring us happiness and blessings. As we prepared for our new bundle of joy, it
+                                            is
+                                            best that we cater for the mother’s health and conditions pre birth. This is why we have
+                                            a
+                                            Maternity Cover that caters for:</p2>
+                                        <ol>
+                                            <li>Normal delivery including professional fees, labour and recovery wards for all
+                                                plans.
+                                            </li>
+                                            <li>Costs of delivery, and other related ailments and complications including ectopic
+                                                pregnancies, miscarriage, etc.
+                                            </li>
+                                            <li>Caesarean section delivery including professional fees, labour and recovery wards.
+                                            </li>
+
+                                        </ol><br>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <!-- BENEFITS -->
+
+                        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+
+                            <div class="job-description text-center">
+                                <div class="job-content">
+                                    <div class="text-justify">
+
+                                        <h2 class="text-center">The cover also provides for enhanced benefits. This include:</h2>
+                                        <ol>
+                                            <li>Cover for Pre-Existing, Chronic, Psychiatric, congenital and HIV/AIDS including
+                                                related
+                                                conditions
+                                            </li>
+                                            <li>Country wide Provider Network
+                                            </li>
+                                            <li>Overseas inpatient referrals covered on credit under listed hospitals
+                                            </li>
+                                            <li>Cover for inpatient dental and optical treatment
+                                            </li>
+                                            <li>Post hospitalization benefit
+                                            </li>
+                                            <li>Funeral expenses benefit
+                                            </li>
+                                            <li>Free Personal Accident benefit for Principle member
+                                            </li>
+                                        </ol>
+
+                                        <hr>
+                                        <h3>Who is eligible for the cover?</h3>
+                                        <ol>
+                                            <li>Adults between the age of 18 years and 75 years.
+                                            </li>
+                                            <li>Children between the age of 1 month and 17 years.
+                                            </li>
+
+                                        </ol><br>
+                                        <hr>
+                                        <h3>Maternity Cover</h3>
+                                        <p2>Children bring us happiness and blessings. As we prepared for our new bundle of joy, it
+                                            is
+                                            best that we cater for the mother’s health and conditions pre birth. This is why we have
+                                            a
+                                            Maternity Cover that caters for:</p2>
+                                        <ol>
+                                            <li>Normal delivery including professional fees, labour and recovery wards for all
+                                                plans.
+                                            </li>
+                                            <li>Costs of delivery, and other related ailments and complications including ectopic
+                                                pregnancies, miscarriage, etc.
+                                            </li>
+                                            <li>Caesarean section delivery including professional fees, labour and recovery wards.
+                                            </li>
+
+                                        </ol><br>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- HOW DO I START -->
+
+                        <div class="tab-pane fade" id="start" role="tabpanel" aria-labelledby="contact-tab">
+
+                            <div class="job-description text-center">
+                                <br><br> <br>
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="load-more text-center ">
+                                            <a href="#" class="btn btn-primary">Get Cover</a>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="load-more text-center ">
+                                            <a href="#" class="btn btn-danger"> Download Sample Policy </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="load-more text-center ">
+                                            <a href="#" class="btn btn-dark">I Need Assistance</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br><br> <br>
+                            </div>
 
 
                         </div>
-                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-                        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
                     </div>
                 </div>
 
-                <div class="job-description text-center">
+                <!-- <div class="job-description text-center">
 
                     <h2>JAMII PLUS </h2>
                     <P class="text-justify">Living a healthy stress free life is every persons dream. A healthy family
@@ -155,7 +416,8 @@
                         </div>
 
                     </div>
-                </div><BR>
+                </div><BR> -->
+                <BR>
             </div>
         </div>
     </div>
