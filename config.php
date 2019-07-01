3943497853ed80da9ea648d452255ed3f375451a
antony-baru <anthonybaru@gmail.com>
2019-07-01 19:01:12 +0300

    slight change to config.php

--- config.php
+++ config.php
@@ -8,6 +8,6 @@ $dbname = "apa";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error) ;
 } 
 ?>
