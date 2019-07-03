4616db2571da1af38e31a3e72cb07d917398c711
peterkinuthia <peterchege442@gmail.com>
2019-07-03 11:56:46 +0300

    clean nav

--- views/nav.php
+++ views/nav.php
@@ -12,26 +12,9 @@
 
 
 
-        <!-- <nav id="nav-menu-container">
-            <ul class="nav-menu">
-                <li class="menu-active"><a href="index.php">HOME</a></li>
-                <li><a href="products.php">WHAT WE OFFER</a></li>
-                <li><a href="about_us.php">ABOUT US</a></li>
-                <li><a href="career.php">CAREERS</a></li>
-                <li><a href="csr.php">CSR</a></li>
-                <li><a href="#">CONTACT US</a></li>
-                <li><a href="portals.php">ONLINE SERVICE</a></li>
-            </ul>
-
-            <div id="flags">
-                <a href="#"><img src="img/kenya.png" alt="APA KENYA"></a>
-                <a href="#"><img src="img/uganda.png" alt="APA UGANDA"></a>
-            </div>
-        </nav> -->
-
-        <nav id="nav-menu-container">
-            <ul class="nav-menu">
-                <li class="menu-active"><a href="index.php">PERSONAL INSURANCE</a></li>
+        <nav id="nav-menu-container" >
+            <ul class="nav-menu ">
+                <li class="menu-active"><a href="#">PERSONAL INSURANCE</a></li>
                 <li><a href="products.php">HEALTH INSURANCE</a></li>
                 <li><a href="#">LIFE ASSURANCE</a></li>
                 <li><a href="career.php">INVESTMENT</a></li>
@@ -46,14 +29,15 @@
             </div>
             <a class="logo1" href="#"><img src="images/Apollo.jpg" width="100" height="auto" alt="" title="" /></a>
         </nav>
-        <!-- #nav-menu-container -->
+
+        <!-- #nav-menu-container-->
 
     </div>
 
     <!-- APOLLO GROUP LOGO -->
-    <!-- <div id="logo1" class="pull-right1"> -->
+        <!-- <div id="logo1" class="pull-right1"> -->
 
-    <!-- </div> -->
+        <!-- </div> -->
 </header>
 
 <!-- #header -->
