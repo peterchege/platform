394d20d86850a92b1e67876c5214a2b4f3d4859f
peterkinuthia <peterchege442@gmail.com>
2019-07-05 14:54:48 +0300

    response on the claim form

--- claim.php
+++ claim.php
@@ -278,7 +278,7 @@ require_once 'inc/functions.php';
 
                             <div class="row book-btn">
                                 <div class="col-md-12"><br>
-                                    <button class="btn btn-primary">SUBMIT CLAIM</button>
+                                    <button class="btn btn-primary" data-target="#modal-close-outside" uk-toggle="target: #modal-close-outside">SUBMIT CLAIM</button>
                                 </div>
                             </div>
                         </div>
@@ -289,102 +289,30 @@ require_once 'inc/functions.php';
         </div>
     </div>
 
+    <!-- ===================================== Claim Remarks ===================================== -->
 
-    <!-- ===================================== APOLLO CENTRE BOOKING FORM ===================================== -->
-
-    <div id="modal-full1" class="uk-modal-full" uk-modal>
-        <div class="uk-modal-dialog">
-            <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
-            <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-top" uk-grid>
-                <div class="uk-background-cover" style="background-image: url('images/shimba/img5.jpg'); background-size: cover;" uk-height-viewport></div>
-                <div class="uk-padding-large">
-
-
-                    <form class="form-container" action="" method="POST">
-                        <div class="form-group col-md-12">
-                            <h3 for="inputAddress">Room Name</h3>
-                            <select name="room_name" id="room_name" class="custom-select custom-select-lg mb-3" value="">
-                                <option disabled selected>Select preferred room</option>
-                                <option value="1">Shimba Hills (Boardroom 18pax)</option>
-                                <option value="2">Taita Hills (10-12pax)</option>
-                                <option value="3">Ngong Hills (Boardroom 8pax)</option>
-                                <option value="4">Chyullu Hills (Auditorium 40pax)</option>
-                                <option value="5">Cherengani Hills (Training Room)</option>
-                                <option value="6">Entertainment Area</option>
-                            </select>
-                        </div>
-
-                        <div class="container">
-                            <div class="row">
-                                <div class="form-group col-md-6">
-                                    <label for="inputEmail4">Start Date</label>
-                                    <input id="start_date" name="start_date" type="date" class="form-control" placeholder="Start date" value="">
-                                </div>
-
-                                <div class="form-group col-md-6">
-                                    <label for="inputPassword4">End Date</label> <input name="end_date" id="end_date" type="date" class="form-control" placeholder="End date" value="">
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="form-group col-md-6">
-                                    <label for="inputEmail4">Start Time</label>
-                                    <input id="start_time" name="start_time" type="time" min="06:00:00" class="form-control" placeholder="Start time" value="">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="inputPassword4">End Time</label>
-                                    <input id="end_time" name="end_time" type="time" max="18:00:00" class="form-control" id="inputPassword4" placeholder="End time" value="">
-                                </div>
-                            </div>
-                        </div>
-
-                        <h3 for="inputAddress" class="comp-detail">COMPANY DETAIL</h3>
-                        <div class="container">
-                            <div class="row">
-                                <div class="form-group col-md-6">
-                                    <label for="inputAddress">Name</label>
-                                    <input name="company_name" id="company_name" type="text" class="form-control" placeholder="Full Name" value="">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="inputAddress2">Phone Number</label>
-                                    <input name="phone" id="phone" type="tel" class="form-control" placeholder="Phone Number" value="">
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="form-group col-md-6">
-                                    <label for="inputAddress">Email Address</label>
-                                    <input name="email" id="email" type="email" class="form-control" placeholder="Email" value="">
-                                </div>
-                                <div class=" form-group col-md-6">
-                                    <label for="inputAddress2">Capacity</label>
-                                    <input name="capacity" id="capacity" type="number" min="1" class="form-control" placeholder="pax" value="">
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="form-group col-md-12">
-                                    <label for="inputAddress">More Information</label>
-                                    <textarea name="more_information" id="more_information" class="form-control"></textarea>
-                                </div>
-                            </div>
-
-                            <div class="row book-btn">
-                                <div class="col-md-12">
-                                    <p id="display"></p>
-                                    <button name="submit" type="submit" class="btn btn-primary" id="submit">BOOK NOW</button>
-                                </div>
-                            </div>
-                        </div>
-
-
-                    </form>
-
+    <!-- This is the modal with the outside close button -->
+    <div id="modal-close-outside" uk-modal>
+        <div class="uk-modal-dialog uk-modal-body">
+            <button class="uk-modal-close-outside" type="button" uk-close></button>
+            <p class="uk-modal-title">
+                <div class="alert alert-success" role="alert">
+                    Sucessfull submited
                 </div>
-            </div>
+</p>
+            <p>Thank you for providing us with detail of your claim. One of our agents will contact you 
+                shortly to guide you through the process. To help us process your claim faster,
+                please download and complete the claim form and prepare the following documentation:
+                    <ol>
+                        <li>Claim form (<a href="#">Click here to download</a>)</li>
+                        <li>Police Abstract</li>
+                        <li>PSV DL</li>
+                        <li>Copy of the Log Book (Insured name)</li>
+                        <li>ID/Passport</li>
+                    </ol>
+            </p>
         </div>
     </div>
-
 
     <!-- loading scripts -->
     <?php
