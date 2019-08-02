82d3d9dfe0a28968b665661d87b13be96a6fdbed
peterkinuthia <peterchege442@gmail.com>
2019-08-02 17:17:08 +0300

    Everlyne Lagat

--- management_asset.php
+++ management_asset.php
@@ -144,7 +144,7 @@
                         <p>
                             Solomon Maoga is the Portfolio Manager at Apollo Asset Management...
                         </p>
-                        <button class="btn btn-primary">Read more</button>
+                        <button class="btn btn-primary" data-target="#modal-full3" uk-toggle>Read more</button>
                     </div>
 
                 </div>
@@ -241,16 +241,16 @@
         </div>
     </div>
 
-    <!-- ===================================== Solomon Maoga ===================================== -->
+    <!-- ===================================== Everlyne Lagat ===================================== -->
 
-    <div id="modal-full2" class="uk-modal-full" uk-modal>
+    <div id="modal-full3" class="uk-modal-full" uk-modal>
         <div class="uk-modal-dialog">
             <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
             <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
-                <div class="uk-background-cover" style="background-image: url('img/managment/solo.jpg');" uk-height-viewport></div>
+                <div class="uk-background-cover" style="background-image: url('img/managment/eva.jpg');" uk-height-viewport></div>
                 <div class="uk-padding-large">
                     <h1>
-                        Solomon Maoga
+                        Everlyne Lagat
                     </h1>
                     <h2 class="small-head">
                         Portfolio Manager
