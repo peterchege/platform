37668abc25ae3ec6d9111418f811425c5bada50b
peterkinuthia <peterchege442@gmail.com>
2019-09-04 17:45:16 +0300

    shimba hills

--- apollo_centre_shimba.php
+++ apollo_centre_shimba.php
@@ -25,7 +25,7 @@
     <!--==========================
     Intro Section
     ============================-->
-    <div class="banner-companies">
+    <div class="shimba-banner">
 
     </div>
     <div class="cover-line">
@@ -41,328 +41,30 @@
 
     <div class="container">
         <div class="apollo">
-            <h1>WELCOME TO APOLLO CENTRE</h1>
-            <div class="under-line img5">
+            <h1>SHIMBA HILLS</h1>
+            <div class="under-line img2">
                 <img src="images/line.png" alt="">
             </div>
-
             <p class="container content-offer wow fadeInUp" data-wow-delay="0.1s">At Apollo centre conference suites, enjoy private meeting
                 place within a commercial building with generous space, spacious balconies opening to every room and ample parking space.
                 The rooms are a hide away for all business and social meetings with exclusivity and convenience for executive individuals,
                 corporate and groups.
             </p>
         </div>
+
     </div>
     <!-- ===================================== OUR ROOMS SLIDESHOW ===================================== -->
-    <div class="container-fluid12">
-        <div class="product product-new">
-
-            <h1 class="text-left  wow fadeInUp" data-wow-delay="0.1s">OUR ROOMS</h1>
-
-            <p class="text-left wow fadeInUp" data-wow-delay="0.1s">Apollo Centre conference suites comprises of meeting rooms of 8 guests,
-                10 guests and an executive boardroom that holds 18 guests. A state of the art auditorium with a modern sitting capacity of
-                40 guests ideal for training and a more relaxed discussion area with video illustrations. Our training room can hold 30 guests
-                on U- shape set up and 44 guests in a classroom setup, the room is ideal for corporate training, promotional training and
-                interactive training. It can also be divided into two rooms for discussion groups.
-
-            </p>
-
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-10 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="center: true">
-
-                            <ul class="uk-slider-items uk-grid">
-                                <li class="uk-width-3-4">
-                                    <div class="uk-panel">
-                                        <img src="images/shimba/img6.jpg" alt="">
-                                        <div class="uk-position-center uk-panel">
-                                            <h1>Shimba Hills</h1>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="uk-width-3-4">
-                                    <div class="uk-panel">
-                                        <img src="images/chyullu/img6.jpg" alt="">
-                                        <div class="uk-position-center uk-panel">
-                                            <h1>Chyullu Hills</h1>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="uk-width-3-4">
-                                    <div class="uk-panel">
-                                        <img src="images/cherengani/img4.jpg" alt="">
-                                        <div class="uk-position-center uk-panel">
-                                            <h1>Cherangani</h1>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="uk-width-3-4">
-                                    <div class="uk-panel">
-                                        <img src="images/Entertainment/img3.jpg" alt="">
-                                        <div class="uk-position-center uk-panel">
-                                            <h1>Entertainment</h1>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-
-                            <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
-                            <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <br>
-
-            <div class="container-fluid">
-
-                <div class="row">
-                    <div class="col-md-4 room-wrapper wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="room-container img-shimba">
-                            <div class="color-overlay-apollo">
-                            </div>
-
-                            <div class="content-centre">
-                                <h1>Shimba Hills<br><small>Boardroom (18px)</small></h1>
-                            </div>
-                        </div>
-
-                        <div class="content-bottom text-justify">
-                            <p>Our world class boardrooms provide the perfect meeting place for executive meetings. With a capacity
-                                of upto 18 persons, they are equipped with an LCD screen & teleconferencing facilities. With varying
-                                degrees of space to fit your needs, you are guaranteed both comfort and privacy.
-                            </p>
-                        </div>
-
-                        <div class="row apollo-btn text-right">
-                            <div class="col-md-6">
-                                <button class="btn btn-primary" data-target="#modal-full" uk-toggle>BOOK NOW</button>
-                            </div>
-                            <div class="col-md-6 text-left">
-                                <a href="apollo_centre_shimba.php">
-                                    <button class="btn btn-primary gallery-btn">VIEW GALLERY</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-4 room-wrapper wow fadeInUp" data-wow-delay="0.2s">
-                        <div class="room-container img-Chyullu">
-                            <div class="color-overlay-apollo">
-                            </div>
-
-                            <div class="content-centre">
-                                <h1>Chyullu Hills<br><small>Auditorium (40px)</small></h1>
-                            </div>
-                        </div>
-
-                        <div class="content-bottom text-justify">
-                            <p>Our auditorium can hold up to 40 people & is equipped with state of the art equipment.
-                                Every room has an LCD screen suitable for presentations. Our training room & the auditorium
-                                have smart boards making them the ideal meeting place.
-                            </p>
-                        </div>
-
-                        <div class="row apollo-btn text-right">
-                            <div class="col-md-6">
-                                <button class="btn btn-primary" data-target="#modal-full1" uk-toggle>BOOK NOW</button>
-                            </div>
-                            <div class="col-md-6 text-left">
-                                <button class="btn btn-primary gallery-btn">VIEW GALLERY</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-4 room-wrapper wow fadeInUp" data-wow-delay="0.3s">
-                        <div class="room-container img-Cherengani">
-                            <div class="color-overlay-apollo">
-                            </div>
-
-                            <div class="content-centre">
-                                <h1>Cherengani Hills<br><small>Training Room</small></h1>
-                            </div>
-                        </div>
-
-                        <div class="content-bottom text-justify">
-                            <p>Our training room can hold 30 guests on U-shape set up and 44 guests in a classroom setup,
-                                the room is ideal for corporate training, promotional training and interactive training.
-                                It can also be divided into two rooms for discussion groups.
-                            </p>
-                        </div>
-
-                        <div class="row apollo-btn text-right">
-                            <div class="col-md-6">
-                                <button class="btn btn-primary" data-target="#modal-full2" uk-toggle>BOOK NOW</button>
-                            </div>
-                            <div class="col-md-6 text-left">
-                                <button class="btn btn-primary gallery-btn">VIEW GALLERY</button>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="row space-up">
-
-                    <div class="col-md-4 room-wrapper wow fadeInUp" data-wow-delay="0.2s">
-                        <div class="room-container img-Chyullu">
-                            <div class="color-overlay-apollo">
-                            </div>
-
-                            <div class="content-centre">
-                                <h1>Ngong Hills<br><small>Boardroom (8px)</small></h1>
-                            </div>
-                        </div>
-
-                        <div class="content-bottom text-justify">
-                            <p>Our auditorium can hold up to 40 people & is equipped with state of the art equipment.
-                                Every room has an LCD screen suitable for presentations. Our training room & the auditorium
-                                have smart boards making them the ideal meeting place.
-                            </p>
-                        </div>
-
-                        <div class="row apollo-btn text-right">
-                            <div class="col-md-6">
-                                <button class="btn btn-primary" data-target="#modal-full1" uk-toggle>BOOK NOW</button>
-                            </div>
-                            <div class="col-md-6 text-left">
-                                <a href="apollo_centre_shimba.php">
-                                    <button class="btn btn-primary gallery-btn">VIEW GALLERY</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-4 room-wrapper wow fadeInUp" data-wow-delay="0.3s">
-                        <div class="room-container img-Cherengani">
-                            <div class="color-overlay-apollo">
-                            </div>
-
-                            <div class="content-centre">
-                                <h1>Taita Hills<br><small>Boardroom (10-12px)</small></h1>
-                            </div>
-                        </div>
-
-                        <div class="content-bottom text-justify">
-                            <p>Our training room can hold 30 guests on U-shape set up and 44 guests in a classroom setup,
-                                the room is ideal for corporate training, promotional training and interactive training.
-                                It can also be divided into two rooms for discussion groups.
-                            </p>
-                        </div>
-
-                        <div class="row apollo-btn text-right">
-                            <div class="col-md-6">
-                                <button class="btn btn-primary" data-target="#modal-full2" uk-toggle>BOOK NOW</button>
-                            </div>
-                            <div class="col-md-6 text-left">
-                                <button class="btn btn-primary gallery-btn">VIEW GALLERY</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-4 room-wrapper wow fadeInUp" data-wow-delay="0.4s">
-                        <div class="room-container img-Entertainment">
-                            <div class="color-overlay-apollo">
-                            </div>
-
-                            <div class="content-centre">
-                                <h1>Entertainment Area</h1>
-                            </div>
-                        </div>
-
-                        <div class="content-bottom text-justify">
-                            <p>The cafeteria has a sitting capacity of 80 guests with dining tables, 100 guests in a
-                                classroom set-up & 150 guests in a cocktail set-up.Tea & lunch is served in our balcony
-                                area covered with sailing canopies for a relaxed atmosphere. Clients also have the option
-                                for self-catering.
-                            </p>
-                        </div>
-
-                        <div class="row apollo-btn text-right">
-                            <div class="col-md-6">
-                                <button class="btn btn-primary" data-target="#modal-full3" uk-toggle>BOOK NOW</button>
-                            </div>
-                            <div class="col-md-6 text-left">
-                                <button class="btn btn-primary gallery-btn">VIEW GALLERY</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-    <!-- =================================== CENTRE AMENITIES ===================================== -->
-
-    <div class="container text-center">
-        <h1 class="wow fadeInUp" data-wow-delay="0.1s">
-            CENTRE AMENITIES
-        </h1>
-        <div class="under-line img6">
-            <img src="images/line.png" alt="">
-        </div>
-        <br>
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-10">
-                <p class="wow fadeInUp" data-wow-delay="0.1s">
-                    With generous space all around, we will give you all the space you need with every room opening up into a large balcony.
-                    Additionally you can access ample parking space that’s convenient and secure.
-                </p>
+            <div class="col-md-4">
+                <div class="gallery-view">
+
+                </div>
+
             </div>
-        </div>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="charter-block">
-                        <img src="images/icon/secure.png" alt="">
-                        <h2>SECURE </h2>
-                        <hr>
-                        <p>
-                            The firm is backed by a strong financial base. Getting into partnership with Swiss Re provides
-                            for a strong financial backing that enables us deliver on our brand promise.
-                        </p>
-
-                    </div>
-                </div>
-
-                <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class=" charter-block">
-                        <img src="images/icon/parking.png" alt="">
-                        <h2>AMPLE PARKING</h2>
-                        <hr>
-                        <p>
-                            Our products and services are well known and respected in the market. We have built our reputation
-                            through hardwork and good service.
-                        </p>
-
-                    </div>
-                </div>
-
-                <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="charter-block">
-                        <img src="images/icon/location.png" alt="">
-                        <h2>CENTRAL LOCATION</h2>
-                        <hr>
-                        <p>
-                            The customer is the focus of our attention. Each account is assigned to a Relationship Officer
-                            who ensures that personal and professional service is given.
-                        </p>
-
-                    </div>
-                </div>
-            </div>
-            <br><br><br>
-
         </div>
 
     </div>
-
 
 
 
