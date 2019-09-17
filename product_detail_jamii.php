b3506c0f3d70a7a704f807efbcba81fd285ff3d6
peterkinuthia <peterchege442@gmail.com>
2019-09-17 12:04:02 +0300

    jamii plus

--- product_detail_jamii.php
+++ product_detail_jamii.php
@@ -829,38 +829,32 @@
                         </div>
                         <hr>
                         <div class="form-group col-md-12">
-                            <h3 for="inputAddress">VEHICLE DETAILS</h3>
+                            <h3 for="inputAddress">COVER DETAILS</h3>
                         </div>
 
                         <div class="container">
                             <div class="row">
-                                <div class="form-group col-md-6">
-                                    <label for="make">Vehicle Make</label>
-                                    <input name="make" type="text" class="form-control" id="make" placeholder="Toyota" value="" required>
+                                <div class="form-group col-md-12">
+                                    <label for="make"> DOB (optional) </label>
+                                    <input name="make" type="date" class="form-control" id="make" value="" required>
                                 </div>
-                                <div class="form-group col-md-6">
-                                    <label for="model">Vehicle Model</label>
-                                    <input name="model" type="text" class="form-control" id="model" placeholder="Crown" value="" required>
+                                <div class="form-group col-md-12">
+                                    <label for="model">Beneficiaries</label>
+                                    <input name="model" type="text" class="form-control" id="model" placeholder="" value="" required>
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="form-group col-md-6">
-                                    <label for="value">Vehicle Value (Kshs)</label>
-                                    <input name="value" type="text" class="form-control" id="value" placeholder="1,400,000" value="" required>
+                                    <label for="value">DOB of Beneficiaries </label>
+                                    <input name="value" type="date" class="form-control" id="value" placeholder="1,400,000" value="" required>
                                 </div>
                                 <div class=" form-group col-md-6">
-                                    <label for="yom">Year of Manufacture</label>
-                                    <input name="yom" type="number" class="form-control" id="yom" placeholder="2019" value="" required data-parsley-pattern="^\d{4}$" data-parsley-trigger="keyup">
+                                    <label for="yom">Relationship of Beneficiaries</label>
+                                    <input name="yom" type="text" class="form-control" id="yom" placeholder="" value="" required data-parsley-pattern="^\d{4}$" data-parsley-trigger="keyup">
                                 </div>
                             </div>
 
-                            <div class="row">
-                                <div class="form-group col-md-12">
-                                    <label for="more_info">More Information</label>
-                                    <textarea name="more_info" id="more_info" class="form-control"></textarea>
-                                </div>
-                            </div>
                             <div style="display: none">
                                 <input type="hidden" id="product_id" name="product_id" value="14">
                                 <input type="hidden" id="product_category_id" name="product_category_id" value="14">
