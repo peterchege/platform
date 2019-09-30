f50cc64cf38cab2eea526d47871ee03d19537ff6
peterkinuthia <peterchege442@gmail.com>
2019-09-30 15:42:11 +0300

    Contact information

---
+++ contact_us.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>APA INSURANCE</title>
+    <link rel="stylesheet" href="css/product.css" />
+    <link rel="stylesheet" href="css/career.css" />
+    <link rel="stylesheet" href="css/contact.css" />
+    <link rel="stylesheet" href="css/apollo_centre.css" media="screen">
+    <link rel="stylesheet" href="css/parsley.css" media="screen">
+
+
+    <?php include 'views/head_links.php'; ?>
+</head>
+
+<body>
+
+
+    <!-- ==============HEADER=============== -->
+    <?php include 'views/nav.php'; ?>
+    <!-- #header -->
+
+
+
+    <!-- ===================================== JOB DESCRIPTION ===================================== -->
+    <div class="container-fluid212">
+        <div class="header-image header-image11 apollo-center">
+        </div>
+    </div>
+
+    <div class="container product-container">
+        <div class="row">
+            <div class="col-12 contact-info-box">
+                <div class="row">
+                    <div class="col-md-5 contact-back">
+                        <div class="content-info">
+                            <h2>CONTACT INFORMATION</h2>
+                            <p><i class="fas fa-street-view"></i> LOCATION : Apollo Centre, Ring Rd Parklands<br>
+                                Westlands Nairobi – Kenya.
+                            </p>
+                            <p><i class="fas fa-phone-square"></i> TELEPHONE : O20 286 2000
+                            </p>
+                            <p><i class="fas fa-mobile"></i> MOBILE NUMBER :(+254) 70991277
+                            </p>
+                            <p><i class="fas fa-envelope-open-text"></i> EMAIL :info@apainsurance.org
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="content-info2">
+                            <h2>DROP US A LINE ! </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- =====================================FOOTER===================================== -->
+    <?php include 'views/footer.php'; ?>
+    <!-- #footer -->
+
+
+
+    <?php
+    require_once 'inc/scripts.php';
+    ?>
+    <script src="js/parsley.min.js"></script>
+    <script src="js/lead.js"></script>
+</body>
+
+</html>
