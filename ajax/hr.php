3414b9487978852baf92b6759f57c4eefb3c352c
anthony-baru <anthonybaru@gmail.com>
2019-10-01 12:10:44 +0300

    uploading files

--- ajax/hr.php
+++ ajax/hr.php
@@ -45,8 +45,12 @@ switch ($_GET['request']) {
             }
 
             if (empty($errors) == true) {
-                move_uploaded_file($file_tmp, "images/" . $email . '-' . $intern_id . '-' . $file_name);
-                echo "Success";
+                $file_path =  "../img/jobsfiles/" . $email . '-' . $intern_id . '-' . $file_name;
+                move_uploaded_file($file_tmp, "../img/jobsfiles/" . $email . '-' . $intern_id . '-' . $file_name);
+                $insert = $db->query("INSERT INTO apa_job_internship_applications(intern_id,first_name,last_name,email,phone,file_path) VALUES('$intern_id','$first_name','$last_name','$email','$phone','')  ");
+                if ($insert) {
+                    echo "Success";
+                }
             } else {
                 print_r($errors);
             }
