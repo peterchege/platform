610352db1ca3bb74a1c596e3bd0cebc243404f39
peterkinuthia <peterchege442@gmail.com>
2019-10-16 18:21:56 +0300

    travel clean up

--- product_detail_travel.php
+++ product_detail_travel.php
@@ -115,7 +115,7 @@
                                                 </div>
                                             </div>
                                             <div class="col-4">
-                                                <div class="load-more text-center " data-target="#modal-full" uk-toggle>
+                                                <div class="load-more text-center " data-target="#modal-full_r" uk-toggle>
                                                     <a href="#" class="btn btn-dark">I Need Assistance</a>
                                                 </div>
                                             </div>
@@ -670,12 +670,12 @@
                                             </div>
                                         </div>
                                         <div class="col-4">
-                                            <div class="load-more text-center " data-target="#modal-full1" uk-toggle="target: #modal-close-outside">
+                                            <div class="load-more text-center " data-target="#modal-full" uk-toggle="target: #modal-close-outside">
                                                 <a href="#" class="btn btn-danger"> Download Forms </a>
                                             </div>
                                         </div>
                                         <div class="col-4">
-                                            <div class="load-more text-center " data-target="#modal-full" uk-toggle>
+                                            <div class="load-more text-center " data-target="#modal-full_r" uk-toggle>
                                                 <a href="#" class="btn btn-dark">I Need Assistance</a>
                                             </div>
                                         </div>
@@ -697,6 +697,53 @@
     <!-- =====================================FOOTER===================================== -->
     <?php include 'views/footer.php'; ?>
     <!-- #footer -->
+
+    <!-- ===================================== MODALS FOR DOWNLOADS ===================================== -->
+    <!-- This is the modal with the outside close button -->
+    <div id="modal-close-outside" uk-modal>
+        <div class="uk-modal-dialog uk-modal-body">
+            <button class="uk-modal-close-outside" type="button" uk-close></button>
+
+            <div class="row forms-download">
+
+                <!-- Browsure form -->
+                <div class="col-md-3 box-download">
+                    <a href="pdf/Jamii_Plus _Brochure.pdf">
+                        <h2> <i class="fas fa-file-pdf"></i> Brochure</h2>
+                        <hr>
+                        <p>Product features & benefits</p>
+                        <hr>
+                        <p class="download">Dowload <i class="fas fa-download"></i></p>
+                    </a>
+                </div>
+
+                <!-- Claim Form -->
+                <div class="col-md-3 box-download">
+                    <a href="#">
+                        <h2> <i class="fas fa-file-pdf"></i> Proposal</h2>
+                        <hr>
+                        <p>Travel cover form </p>
+                        <hr>
+                        <p class="download">Dowload <i class="fas fa-download"></i></p>
+                    </a>
+                </div>
+
+                <!-- Claim Form -->
+                <div class="col-md-3 box-download">
+                    <a href="#">
+                        <h2> <i class="fas fa-file-pdf"></i> Claim Form</h2>
+                        <hr>
+                        <p>Travel Claim <br>form</p>
+                        <hr>
+                        <p class="download">Dowload <i class="fas fa-download"></i></p>
+                    </a>
+                </div>
+
+            </div>
+            <br>
+
+        </div>
+    </div>
 
     <!-- ===================================== LEAD FORM FOR TRAVELL ===================================== -->
 
@@ -768,6 +815,63 @@
                                 <div class="col-md-12">
                                     <button type="submit" name="request" class="btn btn-primary">REQUEST
                                         ASSISTANCE
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ===================================== REQUEST ASSISTANCE FOR COMPREHENSIVE ===================================== -->
+
+    <div id="modal-full_r" class="uk-modal-full" uk-modal>
+        <div class="uk-modal-dialog">
+            <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
+            <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-top" uk-grid>
+                <div class="uk-background-cover" style="background-image: url('img/call_center.jpg'); background-size: cover;" uk-height-viewport></div>
+                <div class="uk-padding-large">
+
+
+                    <form id="form" action="<?= $_SERVER['PHP_SELF']; ?>" method="POST" class="form-container">
+                        <h3 for="inputAddress" class="comp-detail">PERSONAL DETAILS</h3>
+                        <div class="container">
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for="full_name">Full Name</label>
+                                    <input name="full_name" type="text" class="form-control" id="full_name" placeholder="john doe" value="" required>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label for="phone">Mobile Number</label>
+                                    <input name="phone" type="tel" class="form-control" id="phone" placeholder="07xx 232 xxx" value="" required>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for="email">Email Address</label>
+                                    <input name="email" type="email" class="form-control" id="email" placeholder="john@example.com" value="" required data-parsley-type="email" data-parsley-trigger="keyup">
+                                </div>
+                                <div class=" form-group col-md-12">
+                                    <label for="location">Location</label>
+                                    <input name="location" type="text" class="form-control" id="location" placeholder="Nairobi" value="" required>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label for="exampleFormControlTextarea1">Message</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                            </div>
+
+                            <div style="display: none">
+                                <input type="hidden" id="product_id" name="product_id" value="6">
+                                <input type="hidden" id="product_category_id" name="product_category_id" value="14">
+                            </div>
+                            <div class="row book-btn">
+                                <div class="col-md-12">
+                                    <button type="submit" name="request" class="btn btn-primary"> REQUEST ASSISTANCE <i class="fas fa-paper-plane"></i>
                                     </button>
                                 </div>
                             </div>
