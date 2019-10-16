bf6762bb473ce3440683738c6c074545a8f76f4f
peterkinuthia <peterchege442@gmail.com>
2019-10-16 17:19:03 +0300

    jamii plus complete

--- product_detail_motor.php
+++ product_detail_motor.php
@@ -356,7 +356,7 @@
                 <!-- Browsure form -->
                 <div class="col-md-3 box-download">
                     <a href="pdf/Jamii_Plus _Brochure.pdf">
-                        <h2> <i class="fas fa-file-pdf"></i> Browsure</h2>
+                        <h2> <i class="fas fa-file-pdf"></i> Brochure</h2>
                         <hr>
                         <p>Product features & benefits</p>
                         <hr>
