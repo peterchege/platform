4bfc5ab0e262d44f77e90cd4cf6a21b647d7a218
peterkinuthia <peterchege442@gmail.com>
2019-10-16 11:41:30 +0300

    clean up after merge

--- claim_personal_property.php
+++ claim_personal_property.php
@@ -367,13 +367,7 @@
         <div class="uk-modal-dialog">
             <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
             <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-top" uk-grid>
-<<<<<<< HEAD
                 <div class="uk-background-cover" style="background-image: url('img/wiba_claim.jpg'); background-size: cover; background-position: left;" uk-height-viewport></div>
-=======
-                <div class="uk-background-cover"
-                    style="background-image: url('img/group_life_claim.jpg'); background-size: cover;"
-                    uk-height-viewport></div>
->>>>>>> 8ede0a8fa13810ae7399797bfd0cca3b6d33287b
                 <div class="uk-padding-large">
 
 
@@ -457,12 +451,8 @@
         <div class="uk-modal-dialog">
             <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
             <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-top" uk-grid>
-<<<<<<< HEAD
-                <div class="uk-background-cover" style="background-image: url('img/personal_accident.jpg'); background-size: cover;" uk-height-viewport></div>
-=======
                 <div class="uk-background-cover"
                     style="background-image: url('img/critical.jpg'); background-size: cover;" uk-height-viewport></div>
->>>>>>> 8ede0a8fa13810ae7399797bfd0cca3b6d33287b
                 <div class="uk-padding-large">
 
 
@@ -578,13 +568,9 @@
         <div class="uk-modal-dialog">
             <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
             <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-top" uk-grid>
-<<<<<<< HEAD
-                <div class="uk-background-cover" style="background-image: url('img/cow_dead.jpg'); background-size: cover;" uk-height-viewport></div>
-=======
                 <div class="uk-background-cover"
                     style="background-image: url('img/hospitilization.jpg'); background-size: cover;"
                     uk-height-viewport></div>
->>>>>>> 8ede0a8fa13810ae7399797bfd0cca3b6d33287b
                 <div class="uk-padding-large">
 
 
@@ -681,13 +667,9 @@
         <div class="uk-modal-dialog">
             <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
             <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-top" uk-grid>
-<<<<<<< HEAD
-                <div class="uk-background-cover" style="background-image: url('img/crop_dead.jpg'); background-size: cover;" uk-height-viewport></div>
-=======
                 <div class="uk-background-cover"
                     style="background-image: url('img/claim_life3.jpg'); background-size: cover;" uk-height-viewport>
                 </div>
->>>>>>> 8ede0a8fa13810ae7399797bfd0cca3b6d33287b
                 <div class="uk-padding-large">
 
 
