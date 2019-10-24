6f8ddb4cf2d0b6e32adb7eb47ccde86e4db395ff
peterkinuthia <peterchege442@gmail.com>
2019-10-24 15:25:30 +0300

    bonds form

--- product_commercial_bonds.php
+++ product_commercial_bonds.php
@@ -152,13 +152,14 @@
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
-                                                <div class="load-more text-center ">
+                                                <div class="load-more text-center">
                                                     <a href="#" class="btn btn-danger" data-target="#modal-close-outside" uk-toggle="target: #modal-close-outside"> Download Form </a>
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
                                                 <div class="load-more text-center ">
-                                                    <a href="#" class="btn btn-dark" data-target="#modal-full" uk-toggle>I Need Assistance</a>
+                                                    <a href="#" class="btn btn-dark" data-target="#modal-full_r" uk-toggle>I
+                                                        Need Assistance</a>
                                                 </div>
                                             </div>
                                         </div>
@@ -175,12 +176,30 @@
 
                             <div class="job-description text-center">
                                 <div class="job-content">
+                                    <h2>Benefits and Advantages of Bonds
+                                        To The Principals/Obligors</h2>
+                                    <br>
+                                    <ol class="text-left">
+                                        <li>Liquidity of funds</li>
+                                        <li>Multiple Projects</li>
+                                        <li>Convenince</li>
+                                    </ol>
+
+                                    <h2>Benefits and Advantages of Bonds
+                                        To The Owners/Obligees</h2>
+                                    <br>
+                                    <ol class="text-left">
+                                        <li>Faster Commercial Transaction</li>
+                                        <li>Guarantee from a third party</li>
+                                        <li>Economic activity</li>
+                                    </ol>
 
                                     <!-- TABLE OF BENEFITS OUTPATIENT -->
 
                                     <h2>Types of Customs Bonds </h2>
+
                                     <hr>
-                                    <table class="uk-table uk-table-hover uk-table-divider">
+                                    <table class="uk-table uk-table-hover uk-table-divider ">
                                         <thead class="head-text">
                                             <tr>
                                                 <th>Type </th>
@@ -355,7 +374,8 @@
                                         </div>
                                         <div class="col-md-4">
                                             <div class="load-more text-center ">
-                                                <a href="#" class="btn btn-dark" data-target="#modal-full" uk-toggle>I Need Assistance</a>
+                                                <a href="#" class="btn btn-dark" data-target="#modal-full_r" uk-toggle>I
+                                                    Need Assistance</a>
                                             </div>
                                         </div>
                                     </div>
@@ -388,76 +408,42 @@
 
                 <!-- Browsure form -->
                 <div class="col-md-3 box-download">
-                    <!-- <a href="pdf/Jamii_Plus _Brochure.pdf">
-                        <h2>Browsure</h2>
+                    <a href=" pdf/brochure/Jamii Plus Brochure.pdf" target="_blank">
+                        <h2> <i class=" fas fa-file-pdf"></i> Brochure</h2>
                         <hr>
                         <p>Product features & benefits</p>
                         <hr>
-                        <p>Dowload</p>
-                    </a> -->
+                        <p class="download">Dowload <i class="fas fa-download"></i></p>
+
+
+
+                    </a>
                 </div>
 
                 <!-- Claim Form -->
                 <div class="col-md-3 box-download">
-                    <!-- <a href="#">
-                        <h2>Claim Form</h2>
+                    <a href="pdf/claim/Medical_Health_claim_form.pdf">
+                        <h2> <i class="fas fa-file-pdf"></i> Proposal</h2>
                         <hr>
-                        <p>Outpatient Claim Form</p>
+                        <p>Bonds Proposal Form</p>
                         <hr>
-                        <p>Dowload</p>
-                    </a> -->
+                        <p class="download">Dowload <i class="fas fa-download"></i></p>
+
+                    </a>
                 </div>
 
                 <!-- Claim Form -->
                 <div class="col-md-3 box-download">
-                    <!-- <a href="#">
-                        <h2>Calim Form</h2>
+                    <a href="pdf/claim/Medical_Health_claim_form.pdf">
+                        <h2> <i class="fas fa-file-pdf"></i> Claim Form</h2>
                         <hr>
-                        <p>Inpatient Claim form</p>
+                        <p>Bonds Claim <br> form</p>
                         <hr>
-                        <p>Dowload</p>
-                    </a> -->
+                        <p class="download">Dowload <i class="fas fa-download"></i></p>
+                    </a>
                 </div>
 
             </div>
-            <br>
-            <div class="row forms-download">
-
-                <!-- Browsure form -->
-                <div class="col-md-3 box-download">
-                    <!-- <a href="#">
-                        <h2>Claim Form</h2>
-                        <hr>
-                        <p>Dental authorization</p>
-                        <hr>
-                        <p>Dowload</p>
-                    </a> -->
-                </div>
-
-                <!-- Claim Form -->
-                <div class="col-md-3 box-download">
-                    <!-- <a href="#">
-                        <h2>Claim Form</h2>
-                        <hr>
-                        <p>Optical Claim Form</p>
-                        <hr>
-                        <p>Dowload</p>
-                    </a> -->
-                </div>
-
-                <!-- Claim Form -->
-                <div class="col-md-3 box-download">
-                    <!-- <a href="pdf/jamii">
-                        <h2>Calim Form</h2>
-                        <hr>
-                        <p>Inpatient Claim form</p>
-                        <hr>
-                        <p>Dowload</p>
-                    </a> -->
-                </div>
-
-            </div>
-
 
         </div>
     </div>
@@ -520,7 +506,65 @@
                             </div>
                             <div class="row book-btn">
                                 <div class="col-md-12">
-                                    <button type="submit" name="request" class="btn btn-primary">REQUEST ASSISTANCE</button>
+                                    <button type="submit" name="request" class="btn btn-primary">SUBMIT <i class="fas fa-paper-plane"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ===================================== REQUEST ASSISTANCE  ===================================== -->
+
+    <div id="modal-full_r" class="uk-modal-full" uk-modal>
+        <div class="uk-modal-dialog">
+            <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
+            <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-top" uk-grid>
+                <div class="uk-background-cover" style="background-image: url('img/call_center.jpg'); background-size: cover;" uk-height-viewport>
+                </div>
+                <div class="uk-padding-large">
+
+
+                    <form id="form" action="<?= $_SERVER['PHP_SELF']; ?>" method="POST" class="form-container">
+                        <h3 for="inputAddress" class="comp-detail">PERSONAL DETAILS</h3>
+                        <div class="container">
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for="full_name">Full Name</label>
+                                    <input name="full_name" type="text" class="form-control" id="full_name" placeholder="john doe" value="" required>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label for="phone">Mobile Number</label>
+                                    <input name="phone" type="tel" class="form-control" id="phone" placeholder="07xx 245 xxx" value="" required>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for="email">Email Address</label>
+                                    <input name="email" type="email" class="form-control" id="email" placeholder="john@example.com" value="" required data-parsley-type="email" data-parsley-trigger="keyup">
+                                </div>
+                                <div class=" form-group col-md-12">
+                                    <label for="location">Location</label>
+                                    <input name="location" type="text" class="form-control" id="location" placeholder="Nairobi" value="" required>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label for="exampleFormControlTextarea1">Message</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                            </div>
+
+                            <div style="display: none">
+                                <input type="hidden" id="product_id" name="product_id" value="6">
+                                <input type="hidden" id="product_category_id" name="product_category_id" value="14">
+                            </div>
+                            <div class="row book-btn">
+                                <div class="col-md-12">
+                                    <button type="submit" name="request" class="btn btn-primary"> REQUEST ASSISTANCE <i class="fas fa-paper-plane"></i>
+                                    </button>
                                 </div>
                             </div>
                         </div>
