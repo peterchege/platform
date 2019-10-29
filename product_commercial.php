c90c0b22a7eebbf32f135d0f765ab8b9961314cd
peterkinuthia <peterchege442@gmail.com>
2019-10-29 22:22:24 +0300

    special package

--- product_commercial.php
+++ product_commercial.php
@@ -368,7 +368,7 @@
                         <!-- button click -->
                         <div class="row prod-btn">
                             <div class="col-6 text-center">
-                                <a href="#modal-full_mc" uk-toggle class="btn btn-primary">Get Cover</a>
+                                <a href="#modal-full_special" uk-toggle class="btn btn-primary">Get Cover</a>
                             </div>
 
                             <div class="col-6 text-left prod-lf">
