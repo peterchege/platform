08a9941843f4f0dee472bb63981f7ff771fb2eda
Gilbert Kangethu <kangethu2@gmail.com>
2019-11-01 14:46:45 +0300

    Elimu lead form

--- product_detail_life_elimu.php
+++ product_detail_life_elimu.php
@@ -420,20 +420,16 @@
                         <div class="container">
                             <div class="row">
                                 <div class="form-group col-md-6">
-                                    <label for="make">Period/Length to be Covered</label>
-                                    <select id="period" name="period" class="form-control" required>
-                                        <option value="-1" selected disabled>Select...</option>
-                                        <option value="6"> 6 yrs </option>
-                                        <option value="9"> 9 yrs </option>
-                                        <option value="12"> 12 yrs </option>
-                                        <option value="15"> 15 yrs </option>
-                                        <option value="18"> 18 yrs </option>
-                                    </select>
+                                    <label for="period">Period/Length to be Covered</label>
+
+                                    <input id="period" name="period" class="form-control" type="text"
+                                        placeholder="( 5 - 20 yrs )" value="" min="5" max="20" required>
+
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="value">Value of Cover</label>
                                     <input name="value" type="text" class="form-control value" id="value"
-                                        placeholder="( Minimum Ksh 100,000 )" data-parsley-pattern="^.{7,}$" required>
+                                        placeholder="( Minimum Ksh 3,000,000 )" data-parsley-pattern="^.{7,}$" required>
                                 </div>
                             </div>
                             <div class="row">
@@ -456,7 +452,6 @@
                                     </select>
                                 </div>
                             </div>
-
 
                             <div style="display: none">
                                 <input type="hidden" id="product_id" name="product_id" value="20">
