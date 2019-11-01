5bc5863eb808cdb74bfe8de3932217022081407e
anthony-baru <anthonybaru@gmail.com>
2019-11-01 12:14:03 +0300

    added changes to db to incorporate motor trade new field company

--- ajax/leads.php
+++ ajax/leads.php
@@ -62,9 +62,11 @@ switch ($_GET['mode']) {
         $message = ((isset($_POST['message'])) ? filter_var(mysqli_real_escape_string($db, $_POST['message']), FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH) : NULL);
         //afya
         $inpatient = ((isset($_POST['inpatient'])) ? filter_var(mysqli_real_escape_string($db, $_POST['inpatient']), FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_STRIP_HIGH) : 0);
+        //company
+        $company = ((isset($_POST['company'])) ? filter_var(mysqli_real_escape_string($db, $_POST['company']), FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH) : 0);
 
-        $insert = mysqli_query($db, "INSERT INTO products_leads(`leads_product_id`,`leads_product_category_id`,`name`,`email`,`mobile`,`location`,`make`,`model`,`value`,`yom`,`more_info`,`depature_date`,`return_date`,`destination`,`created_at`,`property`,`covers`,`occupation`,`type`,`number`,`population_staff`,`max_take_off_weight`,`geographical_scope`,`pilot_details`,`period`,`age`,`inpatient`,`message`)
-                                            VALUES('$product_id','$product_category_id','$full_name','$email','$phone','$location','$make','$model','$value','$yom','$more_info','$depature_date','$return_date','$destination','$created_at','$property','$cover','$occupation','$type','$number','$population_staff','$max_take_off_weight','$geographical_scope','$pilot_details','$period','$age','$inpatient','$message')");
+        $insert = mysqli_query($db, "INSERT INTO products_leads(`leads_product_id`,`leads_product_category_id`,`name`,`email`,`mobile`,`location`,`make`,`model`,`value`,`yom`,`more_info`,`depature_date`,`return_date`,`destination`,`created_at`,`property`,`covers`,`occupation`,`type`,`number`,`population_staff`,`max_take_off_weight`,`geographical_scope`,`pilot_details`,`period`,`age`,`inpatient`,`message`,`company`)
+                                            VALUES('$product_id','$product_category_id','$full_name','$email','$phone','$location','$make','$model','$value','$yom','$more_info','$depature_date','$return_date','$destination','$created_at','$property','$cover','$occupation','$type','$number','$population_staff','$max_take_off_weight','$geographical_scope','$pilot_details','$period','$age','$inpatient','$message','$company')");
 
         if (!$insert) {
             echo mysqli_error($db);
