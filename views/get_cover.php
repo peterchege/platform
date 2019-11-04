eefb924656f880efc720bf1da633caf29a4b198f
Gilbert Kangethu <kangethu2@gmail.com>
2019-11-04 10:14:55 +0300

    lead form for motor commercial

--- views/get_cover.php
+++ views/get_cover.php
@@ -2510,7 +2510,7 @@ COMMERCIAL COVER
                     <h3 for="inputAddress" class="comp-detail">PERSONAL DETAILS</h3>
                     <div class="container">
                         <div class="row">
-                            <div class="form-group col-md-6">
+                            <!-- <div class="form-group col-md-6">
                                 <label for="full_name">Full Name</label>
                                 <input name="full_name" type="text" class="form-control" id="full_name"
                                     placeholder="John doe" value="" required>
@@ -2569,7 +2569,42 @@ COMMERCIAL COVER
                                 <input name="yom" type="number" class="form-control" id="yom" placeholder="2019"
                                     value="" required data-parsley-pattern="^\d{4}$" data-parsley-trigger="keyup">
                             </div>
+                        </div> -->
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for="full_name">Full Name</label>
+                                    <input name="full_name" type="text" class="form-control" id="full_name"
+                                        placeholder="Full Name" value="" required>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label for="phone">Mobile Number</label>
+                                    <input name="phone" type="tel" class="form-control" id="phone"
+                                        data-parsley-pattern="^(?:254|\+254|0)?(7(?:(?:[129][0-9])|(?:0[0-8])|(4[0-1]))[0-9]{6})$"
+                                        data-parsley-trigger="keyup" placeholder="Mobile Number" value="" required>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for="email">Email Address</label>
+                                    <input name="email" type="email" class="form-control" id="email" placeholder="Email"
+                                        value="" required data-parsley-type="email" data-parsley-trigger="keyup">
+                                </div>
+                                <div class=" form-group col-md-12">
+                                    <label for="location">Location</label>
+                                    <input name="location" type="text" class="form-control" id="location"
+                                        placeholder="Nairobi" value="" required>
+                                </div>
+                                <div class=" form-group col-md-12">
+                                    <label for="occupation">Company</label>
+                                    <input name="company" type="text" class="form-control" id="company"
+                                        placeholder="Company" value="" required>
+                                </div>
+
+                            </div>
+
                         </div>
+
                         <div style="display: none">
                             <input type="hidden" id="product_id" name="product_id" value="26">
                             <input type="hidden" id="product_category_id" name="product_category_id" value="14">
