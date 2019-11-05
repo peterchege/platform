2cde6a812ba8f067f01671d1af030c75834cb360
Gilbert Kangethu <kangethu2@gmail.com>
2019-11-05 11:30:39 +0300

    lead amends

--- views/get_cover.php
+++ views/get_cover.php
@@ -2511,24 +2511,58 @@ COMMERCIAL COVER
 
                     </div>
                     <div class="container"> -->
-                        -->
+                    <div class="row">
+                            <div class="form-group col-md-12">
+                                <label for="full_name">Full Name</label>
+                                <input name="full_name" type="text" class="form-control" id="full_name"
+                                    placeholder="Full Name" value="" required>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label for="phone">Mobile Number</label>
+                                <input name="phone" type="tel" class="form-control" id="phone"
+                                    data-parsley-pattern="^(?:254|\+254|0)?(7(?:(?:[129][0-9])|(?:0[0-8])|(4[0-1]))[0-9]{6})$"
+                                    data-parsley-trigger="keyup" placeholder="Mobile Number" value="" required>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="form-group col-md-12">
+                                <label for="email">Email Address</label>
+                                <input name="email" type="email" class="form-control" id="email" placeholder="Email"
+                                    value="" required data-parsley-type="email" data-parsley-trigger="keyup">
+                            </div>
+                            <div class=" form-group col-md-12">
+                                <label for="location">Location</label>
+                                <input name="location" type="text" class="form-control" id="location"
+                                    placeholder="Nairobi" value="" required>
+                            </div>
+                            <div class=" form-group col-md-12">
+                                <label for="occupation">Company</label>
+                                <input name="company" type="text" class="form-control" id="company"
+                                    placeholder="Company" value="" required>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="container">
 
                         <div style="display: none">
-                            <input type="hidden" id="product_id" name="product_id" value="33">
+                            <input type="hidden" id="product_id" name="product_id" value="37">
                             <input type="hidden" id="product_category_id" name="product_category_id" value="14">
                         </div>
                         <div class="row book-btn">
                             <div class="col-md-12">
-                                <button type="submit" name="request" class="btn btn-primary">SUBMIT <i
-                                        class="fas fa-paper-plane"></i></button>
+                                <button type="submit" name="request" class="btn btn-primary"> SUBMIT <i
+                                        class="fas fa-paper-plane"></i> </button>
                             </div>
                         </div>
                     </div>
-                </form>
+            </form>
 
-            </div>
         </div>
     </div>
+</div>
 </div>
 
 <!-- ===================================== MOTOR COMMERCIAL LEAD FORM ===================================== -->
