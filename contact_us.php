ba455ce2fb799dbca047a7f8043f5a3e532b040f
peterkinuthia <peterchege442@gmail.com>
2019-11-19 11:52:52 +0300

    contact us page image replacement with avatar

--- contact_us.php
+++ contact_us.php
@@ -28,7 +28,7 @@
 
     <!-- ===================================== JOB DESCRIPTION ===================================== -->
     <div class="container-fluid212">
-        <div class="header-image header-image11 apollo-center">
+        <div class="header-image header-image12 feedback-apa">
         </div>
     </div>
 
