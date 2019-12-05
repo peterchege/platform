3500133fab9cef53964bf8b10f455855a70462c1
anthony-baru <anthonybaru@gmail.com>
2019-12-05 11:12:21 +0300

    added date picker with desired values on callback

--- callback.php
+++ callback.php
@@ -16,6 +16,7 @@ require_once 'inc/sessions.php';
     <link rel="stylesheet" href="css/career.css" media="screen">
     <link rel="stylesheet" href="css/customer.css" media="screen">
     <link rel="stylesheet" href="css/parsley.css" media="screen">
+    <link rel="stylesheet" href="css/bootstrap-datepicker3.min.css" media="screen">
 
 
     <?php include 'views/head_links.php'; ?>
@@ -41,7 +42,8 @@ require_once 'inc/sessions.php';
             <div class="under-line img7">
                 <img src="images/line.png" alt="">
             </div>
-            <p class="container content-offer text-left"> Schedule a call on the form below and one fo our customer service
+            <p class="container content-offer text-left"> Schedule a call on the form below and one fo our customer
+                service
                 agent will get back to you shortly.
             </p>
 
@@ -57,13 +59,15 @@ require_once 'inc/sessions.php';
             <div class="col-md-6">
                 <div class="customer-login1">
                     <h2> Fill in the following form :</h2>
-                    <form id="internship_form" method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>" enctype="multipart/form-data" class="form-container">
+                    <form id="internship_form" method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>"
+                        enctype="multipart/form-data" class="form-container">
 
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <label for="full_name">Full Name</label>
-                                    <input name="full_name" type="text" class="form-control" id="full_name" aria-describedby="" placeholder="e.g john" required>
+                                    <input name="full_name" type="text" class="form-control" id="full_name"
+                                        aria-describedby="" placeholder="e.g john" required>
                                 </div>
                             </div>
                         </div>
@@ -72,13 +76,17 @@ require_once 'inc/sessions.php';
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="email">Email address</label>
-                                    <input name="email" type="email" class="form-control" id="email" aria-describedby="" placeholder="e.g john@gmail.com" required>
+                                    <input name="email" type="email" class="form-control" id="email" aria-describedby=""
+                                        placeholder="e.g john@gmail.com" required>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="phone">Phone Number</label>
-                                    <input name="phone" type="tel" class="form-control" id="phone" placeholder="0712 xxx xxx" data-parsley-pattern="^(?:254|\+254|0)?(7(?:(?:[123456789][0-9])|(?:0[0-8])|(4[0-1]))[0-9]{6})$" data-parsley-trigger="keyup" required>
+                                    <input name="phone" type="tel" class="form-control" id="phone"
+                                        placeholder="0712 xxx xxx"
+                                        data-parsley-pattern="^(?:254|\+254|0)?(7(?:(?:[123456789][0-9])|(?:0[0-8])|(4[0-1]))[0-9]{6})$"
+                                        data-parsley-trigger="keyup" required>
                                 </div>
                             </div>
                         </div>
@@ -87,7 +95,8 @@ require_once 'inc/sessions.php';
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="date">Date</label>
-                                    <input name="date" type="date" class="form-control" id="date" aria-describedby="" required>
+                                    <input name="date" type="text" class="form-control" id="date" aria-describedby=""
+                                        required>
                                 </div>
                             </div>
                             <div class="col-md-6">
@@ -104,7 +113,8 @@ require_once 'inc/sessions.php';
                         </div>
                         <div class="customer-btn">
                             <div class="row">
-                                <button type="submit" name="request" class="btn btn-primary">SUBMIT <i class="fas fa-paper-plane"></i> </button>
+                                <button type="submit" name="request" class="btn btn-primary">SUBMIT<i
+                                        class="fas fa-paper-plane"></i> </button>
                             </div>
                         </div>
                     </form>
@@ -126,6 +136,18 @@ require_once 'inc/sessions.php';
     ?>
     <script src="js/parsley.min.js"></script>
     <script src="js/lead.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js" type="text/javascript">
+    </script>
+    <script src="js/bootstrap-datepicker.min.js"> </script>
+    <script>
+        $('#date').datepicker({
+            format: "dd/mm/yyyy",
+            daysOfWeekDisabled: "0,6",
+            autoclose: true,
+            startDate: "today",
+            todayHighlight: true
+        });
+    </script>
 </body>
 
 </html>
