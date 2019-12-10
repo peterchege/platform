bb10f336d2cc9138506b59809299dd02d92e6ec8
peterkinuthia <peterchege442@gmail.com>
2019-12-10 17:51:06 +0300

    careers apa insurance

--- career.php
+++ career.php
@@ -6,7 +6,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>APA INSURANCE</title>
+    <title>Careers - APA INSURANCE</title>
+    <!-- META DATA DESCRIPTION FOR GOOGLE SEARCH RESULT -->
+    <meta name="description" content="Apollo Group, our most valuable assets are our employees. 
+    We pride ourselves in creating experience, delivering results and adding value to all stakeholders. 
+    We advocate for equal employment opportunities to all persons regardless of age, color, nationality, 
+    race, religion, gender and marital status.">
+    <meta name="keywords" content="Building Section,Contents Section,All Risks Section, investment , WIBA Section,  
+    get quote, Home insurance quote, generate quote,">
+    <meta name="author" content="">
+
+    <!-- FACEBOOK MEATADATA -->
+    <meta property="og:url" content="https://www.apainsurance.org/career.php" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Careers - APA INSURANCE" />
+    <meta property="og:description" content=" Apollo Group, our most valuable assets are our employees. 
+    We pride ourselves in creating experience, delivering results and adding value to all stakeholders. 
+    We advocate for equal employment opportunities to all persons regardless of age, color, nationality, 
+    race, religion, gender and marital status." />
+
+    <!-- STYLESHEET -->
     <link rel="stylesheet" href="css/career.css" media="screen">
 
     <?php include 'views/head_links.php'; ?>
@@ -105,14 +124,11 @@
 
 
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
 </body>
 
