6e96c08da0d4bd6e2b143e3387106e9691ba88e1
peterkinuthia <peterchege442@gmail.com>
2019-12-10 15:36:10 +0300

    callback seo

--- callback.php
+++ callback.php
@@ -12,7 +12,22 @@ require_once 'inc/sessions.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>APA INSURANCE</title>
+    <title>Request a Callback - Apa Insurance</title>
+    <!-- META DATA DESCRIPTION FOR GOOGLE SEARCH RESULT -->
+    <meta name="description" content="Schedule a call on the form below and one fo our customer 
+    service agent will get back to you shortly.">
+    <meta name="keywords" content="Building Section,Contents Section,All Risks Section, investment , WIBA Section,  
+    get quote, Home insurance quote, generate quote,">
+    <meta name="author" content="">
+
+    <!-- FACEBOOK MEATADATA -->
+    <meta property="og:url" content="https://www.apainsurance.org/callback.php" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Request a Callback - Apa Insurance" />
+    <meta property="og:description" content=" Schedule a call on the form below and one fo our customer 
+    service agent will get back to you shortly." />
+
+    <!-- STYLESHEET -->
     <link rel="stylesheet" href="css/career.css" media="screen">
     <link rel="stylesheet" href="css/customer.css" media="screen">
     <link rel="stylesheet" href="css/parsley.css" media="screen">
@@ -59,15 +74,13 @@ require_once 'inc/sessions.php';
             <div class="col-md-6">
                 <div class="customer-login1">
                     <h2> Fill in the following form :</h2>
-                    <form id="internship_form" method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>"
-                        enctype="multipart/form-data" class="form-container">
+                    <form id="internship_form" method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>" enctype="multipart/form-data" class="form-container">
 
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <label for="full_name">Full Name</label>
-                                    <input name="full_name" type="text" class="form-control" id="full_name"
-                                        aria-describedby="" placeholder="e.g john" required>
+                                    <input name="full_name" type="text" class="form-control" id="full_name" aria-describedby="" placeholder="e.g john" required>
                                 </div>
                             </div>
                         </div>
@@ -76,17 +89,13 @@ require_once 'inc/sessions.php';
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="email">Email address</label>
-                                    <input name="email" type="email" class="form-control" id="email" aria-describedby=""
-                                        placeholder="e.g john@gmail.com" required>
+                                    <input name="email" type="email" class="form-control" id="email" aria-describedby="" placeholder="e.g john@gmail.com" required>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="phone">Phone Number</label>
-                                    <input name="phone" type="tel" class="form-control" id="phone"
-                                        placeholder="0712 xxx xxx"
-                                        data-parsley-pattern="^(?:254|\+254|0)?(7(?:(?:[123456789][0-9])|(?:0[0-8])|(4[0-1]))[0-9]{6})$"
-                                        data-parsley-trigger="keyup" required>
+                                    <input name="phone" type="tel" class="form-control" id="phone" placeholder="0712 xxx xxx" data-parsley-pattern="^(?:254|\+254|0)?(7(?:(?:[123456789][0-9])|(?:0[0-8])|(4[0-1]))[0-9]{6})$" data-parsley-trigger="keyup" required>
                                 </div>
                             </div>
                         </div>
@@ -95,8 +104,7 @@ require_once 'inc/sessions.php';
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="date">Date</label>
-                                    <input name="date" type="text" class="form-control" id="date" aria-describedby=""
-                                        required>
+                                    <input name="date" type="text" class="form-control" id="date" aria-describedby="" required>
                                 </div>
                             </div>
                             <div class="col-md-6">
@@ -113,8 +121,7 @@ require_once 'inc/sessions.php';
                         </div>
                         <div class="customer-btn">
                             <div class="row">
-                                <button type="submit" name="request" class="btn btn-primary">SUBMIT<i
-                                        class="fas fa-paper-plane"></i> </button>
+                                <button type="submit" name="request" class="btn btn-primary">SUBMIT<i class="fas fa-paper-plane"></i> </button>
                             </div>
                         </div>
                     </form>
