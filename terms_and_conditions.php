7e3b6157d37c778d0dd042a689d9ff6ea2cfe44a
peterkinuthia <peterchege442@gmail.com>
2019-12-13 12:46:26 +0300

    terms and condtions

---
+++ terms_and_conditions.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Terms & Conditions</title>
+    <link rel="stylesheet" href="css/companies.css" media="screen">
+
+    <?php include 'views/head_links.php'; ?>
+
+</head>
+
+
+<body>
+
+    <!--==========================
+    Header
+    ============================-->
+    <?php include 'views/nav.php'; ?>
+    <!-- #header -->
+
+
+
+    <!-- ===================================== ABOUT US ===================================== -->
+
+
+    <div class="container-fluid12">
+        <div class="container apollo app-terms">
+            <BR>
+            <h1 class="wow fadeInUp" data-wow-delay="0.1s">TERMS & CONDITIONS</h1>
+            <div class="under-line img6">
+                <img src="images/line.png" alt="">
+            </div>
+
+            <h2 class="wow fadeInUp" data-wow-delay="0.1s">Introduction </h2>
+
+            <p class="text-justify content-offer wow fadeInUp" data-wow-delay="0.1s">
+                Welcome to the APA Client/Agent App (“App”) privacy policy.
+                APA respects your privacy and is committed to protecting your personal data. This privacy policy aims
+                to give you information on how APA collects and processes your personal data through your use of the App,
+                including any data you may provide through the App when you use some of its functionality.
+                This privacy policy should also be read in conjunction with the App Terms of Use above and is supplementary
+                to the APA Group Companies privacy policy which contains additional details about our collection and use of
+                your personal data, as well as your rights in relation to data we hold about you.
+            </p>
+            <p class="text-justify content-offer wow fadeInUp" data-wow-delay="0.1s">
+                It is important that you read this privacy policy together with any other privacy policy or fair processing
+                notice we may provide on specific occasions in the App when we are collecting or processing personal data
+                about you so that you are fully aware of how and why we are using your data.
+            </p>
+            <h2 class="wow fadeInUp" data-wow-delay="0.1s"> Who We Are </h2>
+
+            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                Your access to the App is facilitated by APA Insurance APA”, “we”, “us” or “our”). We are part of the Apollo group 
+                of companies. Any information that personally identifies you is collected by us as data controller for the purposes 
+                of the Data Protection Laws.
+            </p>
+
+            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                <b>APA INSURANCE:</b> The flagship company, APA Insurance, was formed
+                after the merger of the general businesses of Apollo Insurance Company and Pan Africa Insurance.
+                With a combined experience of over 100 years (Pan Africa – 1946 and Apollo –1977), APA has become
+                the largest insurance firm in Kenya and the most innovative in the product offering. APA Insurance
+                underwrites General Insurance risks such as Motor, Agriculture, Marine, and Micro Insurance.
+                We also underwrite Individual and Family Health Insurance. Our products provide customers with peace
+                of mind, helping both businesses and individuals to return to the same standing following a loss or
+                other unfortunate event.
+            </p>
+
+            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                <b>APA INSURANCE (UGANDA) LIMITED: </b> Our wholly-owned subsidiary underwrites General and Health
+                Insurance. The firms market share and ranking
+                has improved and has been recognised for Best Claims Service by the insurance industry in Uganda.
+            </p>
+
+            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                <b>APA LIFE ASSURANCE LIMITED: </b>APA Life is one of the leading insurance companies in Kenya,
+                providing Life Insurance and Pension Benefits.
+                It has been recognised as the Best Insurance Company in Claims Settlement (Life Business). Our products
+                include Employee Benefits such as Group Life Assurance, Last Expense, Disability Covers and Individual
+                Pension Plans. We also provide Unit Linked Investment Plans and Education Plans, among many others.
+            </p>
+
+            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                <b>APOLLO ASSET MANAGEMENT : </b> Apollo Asset Management Company (AAMC) is focused exclusively on
+                meeting the needs of institutional investors
+                like pension schemes, Saccos, and High Net Worth investors and their families. AAMC is licensed by the
+                Capital
+                Markets Authority (CMA) and the Retirement Benefits Authority (RBA) to conduct Fund Management and
+                Investment
+                Advisory Services.
+            </p>
+
+            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                <b>APOLLO CENTRE: </b>Is wholly owned by Gordon Court Limited, a subsidiary of AIL. It is a commercial
+                building situated in the Westlands
+                area of Nairobi. Besides being the group headquarters, Apollo Centre offers modern conference facilities
+                including private meeting rooms and
+                custom-designed training rooms fitted with state-of-the-art equipment. The facility is popular with several corporates and individual clients
+                due to its exclusivity and convenience.
+            </p>
+
+        </div>
+
+    </div>
+
+
+
+    <!-- =====================================FOOTER===================================== -->
+    <?php include 'views/footer.php'; ?>
+    <!-- #footer -->
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    require_once 'inc/scripts.php';
+    ?>
+
+</body>
+
+</html>
