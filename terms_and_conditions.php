194185da483da7891eb0716620ec505c03ac48fa
peterkinuthia <peterchege442@gmail.com>
2019-12-13 15:32:23 +0300

    terms and condition- who we are

--- terms_and_conditions.php
+++ terms_and_conditions.php
@@ -55,54 +55,20 @@
             <h2 class="wow fadeInUp" data-wow-delay="0.1s"> Who We Are </h2>
 
             <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
-                Your access to the App is facilitated by APA Insurance APA”, “we”, “us” or “our”). We are part of the Apollo group 
-                of companies. Any information that personally identifies you is collected by us as data controller for the purposes 
+                Your access to the App is facilitated by APA Insurance APA”, “we”, “us” or “our”). We are part of the Apollo group
+                of companies. Any information that personally identifies you is collected by us as data controller for the purposes
                 of the Data Protection Laws.
             </p>
 
-            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
-                <b>APA INSURANCE:</b> The flagship company, APA Insurance, was formed
-                after the merger of the general businesses of Apollo Insurance Company and Pan Africa Insurance.
-                With a combined experience of over 100 years (Pan Africa – 1946 and Apollo –1977), APA has become
-                the largest insurance firm in Kenya and the most innovative in the product offering. APA Insurance
-                underwrites General Insurance risks such as Motor, Agriculture, Marine, and Micro Insurance.
-                We also underwrite Individual and Family Health Insurance. Our products provide customers with peace
-                of mind, helping both businesses and individuals to return to the same standing following a loss or
-                other unfortunate event.
+            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                Wherever the term “Apollo Group Companies” is used within these pages this refers to one or more of the trading companies
+                of Apollo and who may or may not offer insurance and financial products or services in relation to the App.
             </p>
 
-            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
-                <b>APA INSURANCE (UGANDA) LIMITED: </b> Our wholly-owned subsidiary underwrites General and Health
-                Insurance. The firms market share and ranking
-                has improved and has been recognised for Best Claims Service by the insurance industry in Uganda.
+            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                For more information concerning Apollo Group visit <a href="https://www.apainsurance.org/index.php"> https://www.apainsurance.org</a>
             </p>
 
-            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
-                <b>APA LIFE ASSURANCE LIMITED: </b>APA Life is one of the leading insurance companies in Kenya,
-                providing Life Insurance and Pension Benefits.
-                It has been recognised as the Best Insurance Company in Claims Settlement (Life Business). Our products
-                include Employee Benefits such as Group Life Assurance, Last Expense, Disability Covers and Individual
-                Pension Plans. We also provide Unit Linked Investment Plans and Education Plans, among many others.
-            </p>
-
-            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
-                <b>APOLLO ASSET MANAGEMENT : </b> Apollo Asset Management Company (AAMC) is focused exclusively on
-                meeting the needs of institutional investors
-                like pension schemes, Saccos, and High Net Worth investors and their families. AAMC is licensed by the
-                Capital
-                Markets Authority (CMA) and the Retirement Benefits Authority (RBA) to conduct Fund Management and
-                Investment
-                Advisory Services.
-            </p>
-
-            <p class="container content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
-                <b>APOLLO CENTRE: </b>Is wholly owned by Gordon Court Limited, a subsidiary of AIL. It is a commercial
-                building situated in the Westlands
-                area of Nairobi. Besides being the group headquarters, Apollo Centre offers modern conference facilities
-                including private meeting rooms and
-                custom-designed training rooms fitted with state-of-the-art equipment. The facility is popular with several corporates and individual clients
-                due to its exclusivity and convenience.
-            </p>
 
         </div>
 
@@ -113,28 +79,6 @@
     <!-- =====================================FOOTER===================================== -->
     <?php include 'views/footer.php'; ?>
     <!-- #footer -->
-
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <?php
     require_once 'inc/scripts.php';
