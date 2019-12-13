4c9c4bf5cd702d5c4bca708621d1a545f5966e82
peterkinuthia <peterchege442@gmail.com>
2019-12-13 16:55:55 +0300

    personal data collected

--- terms_and_conditions.php
+++ terms_and_conditions.php
@@ -35,7 +35,7 @@
             <div class="under-line img6">
                 <img src="images/line.png" alt="">
             </div>
-
+            <!-- Introduction -->
             <h2 class="wow fadeInUp" data-wow-delay="0.1s">Introduction </h2>
 
             <p class="text-justify content-offer wow fadeInUp" data-wow-delay="0.1s">
@@ -52,6 +52,7 @@
                 notice we may provide on specific occasions in the App when we are collecting or processing personal data
                 about you so that you are fully aware of how and why we are using your data.
             </p>
+            <!-- Who We Are -->
             <h2 class="wow fadeInUp" data-wow-delay="0.1s"> Who We Are </h2>
 
             <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
@@ -69,6 +70,47 @@
                 For more information concerning Apollo Group visit <a href="https://www.apainsurance.org/index.php"> https://www.apainsurance.org</a>
             </p>
 
+            <!-- The Data We Collect About You -->
+
+            <h2 class="wow fadeInUp" data-wow-delay="0.1s"> The Data We Collect About You </h2>
+            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+                We may collect, use, store and transfer different kinds of personal data about you which we have grouped together follows:
+            </p>
+            <ul class="text-justify">
+                <li class="wow fadeInUp" data-wow-delay="0.1s"><b>Identity Data</b> includes first name, last name, title, date of birth, vehicle registration number and claim reference number.
+                    We may also incidentally process Identity Data in relation to your passengers if: (1) such data is captured by your device’s
+                    camera and/or microphone when using that feature of the App; and (2) the app photo data is submitted to us by you as part of
+                    a claim, as further described below. </li><br>
+                <li class="wow fadeInUp " data-wow-delay="0.1s"><b>Contact Data </b>includes email address, telephone numbers, address and postcode. </li><br>
+                <li class="wow fadeInUp " data-wow-delay="0.1s"><b>Technical Data</b> includes internet protocol (IP) address, your login data, browser
+                    type and version, time zone setting and location, browser plug-in types and versions, operating system and platform and other
+                    technology on the devices you use to access the App. This includes the journey data, driving data and location data of individual
+                    journeys you track on the App, as further described below. </li><br>
+                <li class="wow fadeInUp " data-wow-delay="0.1s"><b>Profile Data</b> includes your username or similar identifier and password (which may be your
+                    APA username and password and wish to use those login details). </li><br>
+                <li class="wow fadeInUp " data-wow-delay="0.1s"><b>Usage Data</b> includes information about how you use the App and other APA products and services, 
+                    as well as device type, the amount of time you use the App and your use of the App’s features. </li><br>
+                <li class="wow fadeInUp " data-wow-delay="0.1s"><b>Marketing and Communications Data</b> includes your preferences in receiving marketing from us and our 
+                    third parties and your communication preferences.  </li>
+            </ul>
+
+            <!-- How is Your Personal Data Collected  -->
+            <h2 class="wow fadeInUp" data-wow-delay="0.1s"> How is Your Personal Data Collected  </h2>
+            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+               We use different methods to collect data from and about you including through: 
+            </p>
+            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+               <b>Direct interactions.</b> You may give us your Identity, Contact Data and Marketing and Communications Data by completing text fields on the App or 
+               submitting picture footage). This includes personal data you provide when you: 
+            </p>
+            <ul class="text-justify">
+                <li class="wow fadeInUp" data-wow-delay="0.1s">Register for the App or submit login details on the App if you are already registered with APA; </li>
+                <li class="wow fadeInUp " data-wow-delay="0.1s">Obtain a quotation for and/or purchase our products or services;  </li>
+                <li class="wow fadeInUp " data-wow-delay="0.1s">Make a claim following an accident;  </li>
+                <li class="wow fadeInUp " data-wow-delay="0.1s">Submit picture footage to us for the purpose of making a claim (as further described below);</li>
+                <li class="wow fadeInUp " data-wow-delay="0.1s">Give us some feedback. </li>
+            </ul>
+            
 
         </div>
 
