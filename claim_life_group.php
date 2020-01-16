65b618a1c3ed795547789e032b2146abda12d0dc
peterkinuthia <peterchege442@gmail.com>
2020-01-16 17:12:26 +0300

    criticall illnesss

--- claim_life_group.php
+++ claim_life_group.php
@@ -109,7 +109,7 @@
 
                     <div class="col-md-4 wow fadeInUp" data-wow-delay="0.4s">
                         <div class="product-box we img-critical">
-                            <a href="#modal-full3" uk-toggle>
+                            <a href="claim_report_life_group_critical_illness.php">
                                 <div class="color-overlay"></div>
                                 <div class="wrapper">
                                     <div class="content">
