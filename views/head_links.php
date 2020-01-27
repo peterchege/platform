f19e8ed8a30b5d6e254840e5cba28a7dcd4f723e
peterkinuthia <peterchege442@gmail.com>
2020-01-27 19:02:09 +0300

    google tag manager

--- views/head_links.php
+++ views/head_links.php
@@ -53,6 +53,25 @@
   gtag('config', 'UA-151247905-1');
 </script>
 
+<!-- Google Tag Manager -->
+<script>
+  (function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src =
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-KQGRFJK');
+</script>
+<!-- End Google Tag Manager -->
+
 <!-- vue js CDN -->
 <script src="https://cdn.jsdelivr.net/npm/vue@2.6.0"></script>
 
--- views/nav.php
+++ views/nav.php
@@ -343,4 +343,6 @@
     </div>
 
 </header>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQGRFJK" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
