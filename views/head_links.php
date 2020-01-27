866301b53f1fbd05040b75f9d1201347b33264e3
peterkinuthia <peterchege442@gmail.com>
2020-01-27 18:42:39 +0300

    quantcast update

--- views/head_links.php
+++ views/head_links.php
@@ -83,26 +83,26 @@
 <!-- End Facebook Pixel Code -->
 
 <!-- Quantcast Tag -->
-<script type="text/javascript">
-  var _qevents = _qevents || [];
+  <script type="text/javascript">
+    var _qevents = _qevents || [];
 
-  (function() {
-    var elem = document.createElement('script');
-    elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
-    elem.async = true;
-    elem.type = "text/javascript";
-    var scpt = document.getElementsByTagName('script')[0];
-    scpt.parentNode.insertBefore(elem, scpt);
-  })();
+    (function() {
+      var elem = document.createElement('script');
+      elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+      elem.async = true;
+      elem.type = "text/javascript";
+      var scpt = document.getElementsByTagName('script')[0];
+      scpt.parentNode.insertBefore(elem, scpt);
+    })();
 
-  _qevents.push({
-    qacct: "p-21dGAYzXnjr9R"
-  });
-</script>
+    _qevents.push({
+      qacct: "p-21dGAYzXnjr9R"
+    });
+  </script>
 
-<noscript>
-  <div style="display:none;">
-    <img src="//pixel.quantserve.com/pixel/p-21dGAYzXnjr9R.gif" border="0" height="1" width="1" alt="Quantcast" />
-  </div>
-</noscript>
-<!-- End Quantcast tag -->
+  <noscript>
+    <div style="display:none;">
+      <img src="//pixel.quantserve.com/pixel/p-21dGAYzXnjr9R.gif" border="0" height="1" width="1" alt="Quantcast" />
+    </div>
+  </noscript>
+  <!-- End Quantcast tag -->
