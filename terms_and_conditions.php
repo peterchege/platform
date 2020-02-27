2f99abb669ac2d6bcc451442e6d268bb2a34a40f
peterkinuthia <peterchege442@gmail.com>
2020-02-27 17:55:37 +0300

    removed animations

--- terms_and_conditions.php
+++ terms_and_conditions.php
@@ -31,14 +31,14 @@
     <div class="container-fluid12">
         <div class="container apollo app-terms">
             <BR>
-            <h1 class="wow fadeInUp" data-wow-delay="0.1s">TERMS & CONDITIONS</h1>
+            <h1>TERMS & CONDITIONS</h1>
             <div class="under-line img6">
                 <img src="images/line.png" alt="">
             </div>
             <!-- Introduction -->
-            <h2 class="wow fadeInUp" data-wow-delay="0.1s">Introduction </h2>
+            <h2 >Introduction </h2>
 
-            <p class="text-justify content-offer wow fadeInUp" data-wow-delay="0.1s">
+            <p class="text-justify content-offer">
                 Welcome to the APA Client/Agent App (“App”) privacy policy.
                 APA respects your privacy and is committed to protecting your personal data. This privacy policy aims
                 to give you information on how APA collects and processes your personal data through your use of the App,
@@ -47,93 +47,93 @@
                 to the APA Group Companies privacy policy which contains additional details about our collection and use of
                 your personal data, as well as your rights in relation to data we hold about you.
             </p>
-            <p class="text-justify content-offer wow fadeInUp" data-wow-delay="0.1s">
+            <p class="text-justify content-offer">
                 It is important that you read this privacy policy together with any other privacy policy or fair processing
                 notice we may provide on specific occasions in the App when we are collecting or processing personal data
                 about you so that you are fully aware of how and why we are using your data.
             </p>
             <!-- Who We Are -->
-            <h2 class="wow fadeInUp" data-wow-delay="0.1s"> Who We Are </h2>
+            <h2 > Who We Are </h2>
 
-            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+            <p class="content-offer">
                 Your access to the App is facilitated by APA Insurance APA”, “we”, “us” or “our”). We are part of the Apollo group
                 of companies. Any information that personally identifies you is collected by us as data controller for the purposes
                 of the Data Protection Laws.
             </p>
 
-            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+            <p class="content-offer ">
                 Wherever the term “Apollo Group Companies” is used within these pages this refers to one or more of the trading companies
                 of Apollo and who may or may not offer insurance and financial products or services in relation to the App.
             </p>
 
-            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+            <p class="content-offer ">
                 For more information concerning Apollo Group visit <a href="https://www.apainsurance.org/index.php"> https://www.apainsurance.org</a>
             </p>
 
             <!-- The Data We Collect About You -->
 
-            <h2 class="wow fadeInUp" data-wow-delay="0.1s"> The Data We Collect About You </h2>
-            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+            <h2 > The Data We Collect About You </h2>
+            <p class="content-offer ">
                 We may collect, use, store and transfer different kinds of personal data about you which we have grouped together follows:
             </p>
             <ul class="text-justify">
-                <li class="wow fadeInUp" data-wow-delay="0.1s"><b>Identity Data</b> includes first name, last name, title, date of birth, vehicle registration number and claim reference number.
+                <li ><b>Identity Data</b> includes first name, last name, title, date of birth, vehicle registration number and claim reference number.
                     We may also incidentally process Identity Data in relation to your passengers if: (1) such data is captured by your device’s
                     camera and/or microphone when using that feature of the App; and (2) the app photo data is submitted to us by you as part of
                     a claim, as further described below. </li><br>
-                <li class="wow fadeInUp " data-wow-delay="0.1s"><b>Contact Data </b>includes email address, telephone numbers, address and postcode. </li><br>
-                <li class="wow fadeInUp " data-wow-delay="0.1s"><b>Technical Data</b> includes internet protocol (IP) address, your login data, browser
+                <li><b>Contact Data </b>includes email address, telephone numbers, address and postcode. </li><br>
+                <li><b>Technical Data</b> includes internet protocol (IP) address, your login data, browser
                     type and version, time zone setting and location, browser plug-in types and versions, operating system and platform and other
                     technology on the devices you use to access the App. This includes the journey data, driving data and location data of individual
                     journeys you track on the App, as further described below. </li><br>
-                <li class="wow fadeInUp " data-wow-delay="0.1s"><b>Profile Data</b> includes your username or similar identifier and password (which may be your
+                <li><b>Profile Data</b> includes your username or similar identifier and password (which may be your
                     APA username and password and wish to use those login details). </li><br>
-                <li class="wow fadeInUp " data-wow-delay="0.1s"><b>Usage Data</b> includes information about how you use the App and other APA products and services,
+                <li><b>Usage Data</b> includes information about how you use the App and other APA products and services,
                     as well as device type, the amount of time you use the App and your use of the App’s features. </li><br>
-                <li class="wow fadeInUp " data-wow-delay="0.1s"><b>Marketing and Communications Data</b> includes your preferences in receiving marketing from us and our
+                <l><b>Marketing and Communications Data</b> includes your preferences in receiving marketing from us and our
                     third parties and your communication preferences. </li>
             </ul>
 
             <!-- How is Your Personal Data Collected  -->
-            <h2 class="wow fadeInUp" data-wow-delay="0.1s"> How is Your Personal Data Collected </h2>
-            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+            <h2 > How is Your Personal Data Collected </h2>
+            <p class="content-offer ">
                 We use different methods to collect data from and about you including through:
             </p>
-            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+            <p class="content-offer ">
                 <b>Direct interactions.</b> You may give us your Identity, Contact Data and Marketing and Communications Data by completing text fields on the App or
                 submitting picture footage). This includes personal data you provide when you:
             </p>
             <ul class="text-justify">
-                <li class="wow fadeInUp" data-wow-delay="0.1s">Register for the App or submit login details on the App if you are already registered with APA; </li>
-                <li class="wow fadeInUp " data-wow-delay="0.1s">Obtain a quotation for and/or purchase our products or services; </li>
-                <li class="wow fadeInUp " data-wow-delay="0.1s">Make a claim following an accident; </li>
-                <li class="wow fadeInUp " data-wow-delay="0.1s">Submit picture footage to us for the purpose of making a claim (as further described below);</li>
-                <li class="wow fadeInUp " data-wow-delay="0.1s">Give us some feedback. </li>
+                <li>Register for the App or submit login details on the App if you are already registered with APA; </li>
+                <li>Obtain a quotation for and/or purchase our products or services; </li>
+                <li>Make a claim following an accident; </li>
+                <li>Submit picture footage to us for the purpose of making a claim (as further described below);</li>
+                <li>Give us some feedback. </li>
             </ul>
 
             <!-- Automated Technologies or Interactions -->
-            <h2 class="wow fadeInUp" data-wow-delay="0.1s">Automated Technologies or Interactions </h2>
+            <h2 >Automated Technologies or Interactions </h2>
             <ul class="text-justify">
-                <li class="wow fadeInUp" data-wow-delay="0.1s">As you interact with the App, we may automatically collect Profile Data, Usage Data and Technical Data.
+                <li>As you interact with the App, we may automatically collect Profile Data, Usage Data and Technical Data.
                     We collect this personal data by using cookies, server logs, diagnostic information, communications logs and other similar technologies. </li><br>
-                <li class="wow fadeInUp " data-wow-delay="0.1s">We will only process your location data where we have your explicit consent to do so. You will also have
+                <li>We will only process your location data where we have your explicit consent to do so. You will also have
                     the option via your device’s settings to withdraw such consent at any time. </li>
             </ul>
 
 
             <!-- Purposes for Which We Will Use Your Personal Data -->
 
-            <h2 class="wow fadeInUp" data-wow-delay="0.1s">Purposes for Which We Will Use Your Personal Data </h2>
-            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+            <h2 >Purposes for Which We Will Use Your Personal Data </h2>
+            <p class="content-offer">
                 We have set out below, in a table format, a description of all the ways we plan to use your personal data, and which of the legal bases we rely on to do so.
                 We have also identified what our legitimate interests are where appropriate.
             </p>
-            <p class="content-offer wow fadeInUp text-left" data-wow-delay="0.1s">
+            <p class="content-offer">
                 Note that we may process your personal data for more than one lawful ground depending on the specific purpose for which we are using your data. Please contact
                 us using the details below if you need details about the specific legal ground we are relying on to process your personal data where more than one ground has
                 been set out in the table below.
             </p>
-            <div class="terms-table wow fadeInUp" data-wow-delay="0.1s">
+            <div class="terms-table">
                 <table class="uk-table uk-table-striped">
                     <thead>
                         <tr>
