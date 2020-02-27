6bd191b1ab1992d4bdd305c9224a303ce8c0b268
peterkinuthia <peterchege442@gmail.com>
2020-02-27 17:43:04 +0300

    disclamer completed

---
+++ disclamer.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>DISCLAIMER - APA Insurance</title>
+    <link rel="stylesheet" href="css/companies.css" media="screen">
+
+    <?php include 'views/head_links.php'; ?>
+
+</head>
+
+
+<body>
+
+    <!--==========================
+    Header
+    ============================-->
+    <?php include 'views/nav.php'; ?>
+    <!-- #header -->
+
+
+
+    <!-- ===================================== ABOUT US ===================================== -->
+
+
+    <div class="container-fluid12">
+        <div class="container apollo app-terms">
+            <BR>
+            <h1>DISCLAIMER</h1>
+            <div class="under-line img2">
+                <img src="images/line.png" alt="">
+            </div>
+            <!-- Introduction -->
+
+            <p class="text-justify content-offer">
+                To find out more about this product or any other product, speak to our customer service
+                representatives in any of our branches. Whilst these products have been approved by the
+                regulator, Capital market Authority does not take responsibility for the financial soundness
+                of the scheme or the correctness of any statement made or opinions expressed in this regard.
+            </p>
+            <p class="text-justify content-offer">
+                Past performance is not necessarily is a guide to the future investment performance. Here are
+                no guarantees on the investor’s capital as the value of the investment can fall as well as rise
+                depending on the performance of underlying investments.
+            </p>
+            <p class="text-justify content-offer">
+                The price of Apollo Equity Fund, Apollo Balanced Fund and the yield of the Apollo Money Market
+                Fund may change on a daily basis. Prices of the units can fall as well as rise as a result of
+                changes in the value of the underlying investment.
+            </p>
+            <p class="text-justify content-offer">
+                PInvestors should refer to information memorandum and other incorporation documents available
+                at the offices of the Fund Management for full terms and conditions attaching to their investment
+                in the Unit Trust Funds. To find out more about this product or any other product, speak to our
+                customer service representatives in any of our four branches. The price of shares, and the income
+                therefrom if the collective investment scheme pays dividends, may go down as well as up and investors
+                are reminded that.
+            </p>
+        </div>
+
+    </div>
+
+
+
+    <!-- =====================================FOOTER===================================== -->
+    <?php include 'views/footer.php'; ?>
+    <!-- #footer -->
+
+    <?php
+    require_once 'inc/scripts.php';
+    ?>
+
+</body>
+
+</html>
